2646572cb88eb10a541f6aabec34c46a3a984cc1
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-14 20:30:05 +0100

    Added players table

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -10,5 +10,24 @@
     echo "<button onclick='show_encounters()' id='button_encounters'>Encounters</button>";
     echo "<a href='".site_url()."' class='logout'>Logout</a>";
     echo "<br/><br/>";
+
+
+    echo "<div id='players'>";
+    echo "<table><thead><tr><th>Id</th><th>Name</th><th>Last Raid</th><th>Last Loot</th><th>Points</th><th>&nbsp;</th><th>&nbsp;</th></tr></thead>";
+    for ($i = 0; $i < count($players_list); $i++) {
+        $player = $players_list[$i];
+        
+        echo "<tr><td>".$player['id']."</td>";
+        echo "<td>".$player['name']."</td>";
+        echo "<td>".$player['last_raid']."</td>";
+        echo "<td>".$player['last_loot']."</td>";
+        echo "<td>".$player['points']."</td>";
+        echo "<td><a href='".site_url()."/players/delete/".$player['id']."' class='delete'>Delete</a></td>";
+        echo "<td><a href='".site_url()."/players/show_modify/".$player['id']."' class='modify'>Modify</a></td></tr>";
+    }
+    echo "</table><br/>";
+    echo "<a href='".site_url()."/players/show_insert/' class='insert'>New</a>";
+    echo "<br/><br/>";
+    echo "</div>";
             
 ?>
