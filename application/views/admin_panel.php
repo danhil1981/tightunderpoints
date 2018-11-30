b352de6c15912a8f2aab46ea621261539179b5d9
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 10:29:04 +0100

    removed unused table columns, modified h1

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -1,6 +1,6 @@
 <?php
 
-    echo "<h1>Tight Underpoints</h1>";
+    echo "<h1 class='text-center text-white'>Admin Panel</h1>";
     echo "<br/><br/>";
     if(isset($msg)) echo $msg;
     echo "<br/><br/>";
@@ -8,20 +8,18 @@
     echo "<button onclick='show_raids()' id='button_raids'>Raids</button>";
     echo "<button onclick='show_items()' id='button_items'>Items</button>";
     echo "<button onclick='show_encounters()' id='button_encounters'>Encounters</button>";
+    echo anchor('users/admin_panel', 'Refresh', 'class="refresh"');
     echo "<a href='".site_url()."' class='logout'>Logout</a>";
     echo "<br/><br/>";
 
 
     echo "<div id='players'>";
-    echo "<table><thead><tr><th>Id</th><th>Name</th><th>Last Raid</th><th>Last Loot</th><th>Points</th><th>&nbsp;</th><th>&nbsp;</th></tr></thead>";
+    echo "<table class='table-dark'><thead><tr><th>Id</th><th>Name</th><th>&nbsp;</th><th>&nbsp;</th></tr></thead>";
     for ($i = 0; $i < count($players_list); $i++) {
         $player = $players_list[$i];
         
         echo "<tr><td>".$player['id']."</td>";
         echo "<td>".$player['name']."</td>";
-        echo "<td>".$player['last_raid']."</td>";
-        echo "<td>".$player['last_loot']."</td>";
-        echo "<td>".$player['points']."</td>";
         echo "<td><a href='".site_url()."/players/delete/".$player['id']."' class='delete'>Delete</a></td>";
         echo "<td><a href='".site_url()."/players/show_modify/".$player['id']."' class='modify'>Modify</a></td></tr>";
     }
