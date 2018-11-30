1c60bd9698cc4b81aaff9bddaeb235488a609c7c
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 19:35:12 +0100

    changes to javascript calls, minor design changes

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -1,25 +1,21 @@
 <h1 class='text-center text-white'>Admin Panel</h1>
 <br/><br/>
 
-<?php if(isset($msg)) echo $msg ?>
-
-<br/><br/>
-
 <div class="row">
     <div class="col-10 offset-1">
-        <button onclick='show_users()' id='button_users' class="btn btn-primary">Users</button>
-        <button onclick='show_players()' id='button_players' class="btn">Players</button>
-        <button onclick='show_characters()' id='button_characters' class="btn">Characters</button>
-        <button onclick='show_raids()' id='button_raids' class="btn">Raids</button>
-        <button onclick='show_events()' id='button_events' class="btn">Events</button>
-        <button onclick='show_bosses()' id='button_bosses' class="btn">Bosses</button>
-        <button onclick='show_items()' id='button_items' class="btn">Items</button>
-        <button onclick='show_drops()' id='button_drops' class="btn">Drops</button>
-        <button onclick='show_attendance()' id='button_attendance' class="btn">Attendence</button>
-        <button onclick='show_loots()' id='button_loots' class="btn">Loots</button>
-
+        <?php if(isset($msg)) echo $msg ?>
+        <br/><br/>
+        <button onclick='show("users")' id='button_users' class="btn btn-primary">Users</button>
+        <button onclick='show("players")' id='button_players' class="btn">Players</button>
+        <button onclick='show("characters")' id='button_characters' class="btn">Characters</button>
+        <button onclick='show("raids")' id='button_raids' class="btn">Raids</button>
+        <button onclick='show("events")' id='button_events' class="btn">Events</button>
+        <button onclick='show("bosses")' id='button_bosses' class="btn">Bosses</button>
+        <button onclick='show("items")' id='button_items' class="btn">Items</button>
+        <button onclick='show("drops")' id='button_drops' class="btn">Drops</button>
+        <button onclick='show("attendance")' id='button_attendance' class="btn">Attendence</button>
+        <button onclick='show("loots")' id='button_loots' class="btn">Loots</button>
         <?php echo anchor('users/admin_panel', 'Refresh', 'class="btn btn-success"');?>
-
         <a href='<?php echo site_url()?>' class='btn btn-danger'>Logout</a>
         <br/><br/>
     </div>
@@ -27,8 +23,8 @@
 
 <div class="row">
     <div class="col-8 offset-2">
-        <div id='users' class="visible">
-            <table class='table-dark table-responsive'>
+        <div id='users'>
+            <table class='table-dark'>
                 <thead>
                     <tr>
                         <th>Id</th>
@@ -48,8 +44,12 @@
                     }
                 ?>
             </table>
+            <br/>
+            <a href='<?php echo site_url()?>/users/show_insert/' class='btn btn-success btn-sm'>New</a>
+            <br/><br/>
+        </div>
 
-        <div id='players' class="invisible">
+        <div id='players' style='display: none;'>
             <table class='table-dark'>
                 <thead>
                     <tr>
@@ -65,14 +65,20 @@
                 
                         echo "<tr><td>".$player['id']."</td>";
                         echo "<td>".$player['name']."</td>";
-                        echo "<td><a href='".site_url()."/players/delete/".$player['id']."' class='delete'>Delete</a></td>";
-                        echo "<td><a href='".site_url()."/players/show_modify/".$player['id']."' class='modify'>Modify</a></td></tr>";
+                        echo "<td><a href='".site_url()."/players/delete/".$player['id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
+                        echo "<td><a href='".site_url()."/players/show_modify/".$player['id']."' class='btn btn-warning btn-sm''>Modify</a></td></tr>";
                     }
                 ?>
             </table>
             <br/>
-            <a href='<?php echo site_url()?>/players/show_insert/' class='insert'>New</a>
+            <a href='<?php echo site_url()?>/players/show_insert/' class='btn btn-success btn-sm'>New</a>
             <br/><br/>
         </div>
     </div>
 </div>
+
+<?php 
+    if (isset($table_to_show)) {
+        echo "<script>show('".$table_to_show."');</script>";
+    }
+?>
