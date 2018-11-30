d50f96acddf659c785c0492e59f463b3e62f326a
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 10:30:24 +0100

    redesign, implemented bootstrap

--- application/views/form_login.php
+++ application/views/form_login.php
@@ -2,12 +2,29 @@
     $this->session->userdata = array();
 
     if(isset($msg)) echo $msg;
-    echo "<h1>Tight Underpoints</h1>";
-    echo "<h2>Login</h2>";
-    echo form_open('users/process_login');
-    echo "User: ".form_input('user')."<br/>";
-    echo "Password: ".form_password('password')."<br/>";
-    echo form_submit('submit', 'Submit');
-    echo form_close();
-
 ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <h1 class='text-center text-white'>Login</h1>
+
+                <?php echo form_open('users/process_login');?>
+                <div class="form-group">
+                    <div class="text-center text-white">
+                        User<br/>
+                    </div>
+                    <?php echo form_input('user','','class="form-control"')?>
+                </div>
+                <div class="form-group">
+                    <div class="text-center text-white">
+                        Password<br/>
+                    </div>
+                    <?php echo form_password('password','','class="form-control"')?>
+                </div>
+                <div class="text-center">
+                    <?php echo form_submit('submit', 'Submit', 'class="btn btn-success"');?>
+                    <?php echo form_close();?>
+            </div>
+        </div> 
+    </div>
+
