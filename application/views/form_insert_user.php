e115fef31155937f2b7d5a6bbb6f9d18f2d8dc57
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 19:32:23 +0100

    created forms to insert and modify users

---
+++ application/views/form_insert_user.php
@@ -0,0 +1,23 @@
+    
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6 offset-3">
+                        <h1 class="text-center text-white">New User</h1>
+
+                        <br/><br/>
+            
+                        <?php echo form_open_multipart('users/insert');?>
+
+                        <div class="form-group">
+                            <div class="text-white">
+                                Name<br/><br/>
+                            </div>
+                            <?php echo form_input('name','','required class="form-control"')?>
+                            <br/><br/>
+                        </div>
+                        <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
+                        <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
+                        <?php echo form_close();?>
+                    </div>
+                </div>
+            </div>
