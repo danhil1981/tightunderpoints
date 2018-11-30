f1155afa8a60dd7c50df1445802f8311d378a028
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 19:33:03 +0100

    changed design, added bootstrap

--- application/views/form_insert_player.php
+++ application/views/form_insert_player.php
@@ -1,11 +1,23 @@
-<?php
-
-    echo "<h1>New player</h1>";
-    echo anchor('users/admin_panel', 'Cancel', 'class="cancel"');
-    echo "<br/><br/>";
-    echo form_open_multipart('players/insert');
-    echo "Name: ".form_input('name','','required')."<br/>";
-    echo form_submit('submit', 'Submit');
-    echo form_close();
     
-?>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6 offset-3">
+                        <h1 class="text-center text-white">New Player</h1>
+
+                        <br/><br/>
+            
+                        <?php echo form_open_multipart('players/insert');?>
+
+                        <div class="form-group">
+                            <div class="text-white">
+                                Name<br/><br/>
+                            </div>
+                            <?php echo form_input('name','','required class="form-control"')?>
+                            <br/><br/>
+                        </div>
+                        <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
+                        <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
+                        <?php echo form_close();?>
+                    </div>
+                </div>
+            </div>
--- application/views/form_modify_player.php
+++ application/views/form_modify_player.php
@@ -1,12 +1,24 @@
-<?php
+    <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6 offset-3">
+                        <h1 class="text-center text-white">Modify Player</h1>
 
-    echo "<h1>Modify Player</h1>";
-    echo anchor('users/admin_panel', 'Cancel', 'class="cancel"');
-    echo "<br/><br/>";
-    echo form_open_multipart('players/modify');
-    echo "Name: ".form_input('name',$player['name'],'required')."<br/>";
-    echo form_hidden('id',$player['id']);
-    echo form_submit('submit', 'Submit');
-    echo form_close();
-    
-?>
+                        <br/><br/>
+            
+                        <?php echo form_open_multipart('players/modify');?>
+
+                        <div class="form-group">
+                            <div class="text-white">
+                                Name<br/><br/>
+                            </div>
+                            <?php echo form_input('name',$player['name'],'required class="form-control"')?>
+                            <br/><br/>
+                        </div>
+                        <?php echo form_hidden('id',$player['id']);?>
+                        <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
+                        <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
+                        <?php echo form_close();?>
+                    </div>
+                </div>
+            </div>
+            
