767b912b9938feaccf4a0011efba95c25b4d25d5
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 20:21:38 +0100

    Added password and type fields to the form

--- application/views/form_insert_user.php
+++ application/views/form_insert_user.php
@@ -13,11 +13,25 @@
                                 Name<br/><br/>
                             </div>
                             <?php echo form_input('name','','required class="form-control"')?>
-                            <br/><br/>
                         </div>
-                        <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
-                        <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
-                        <?php echo form_close();?>
+                        <div class="form-group">
+                            <div class="text-white">
+                                Password<br/><br/>
+                            </div>
+                            <?php echo form_password('password','','required class="form-control"')?>
+                        </div>
+                        <?php $options = array('1' => 'Admin','2' => 'Officer','3' => 'Member');?>
+                        <div class="form-group">
+                            <div class="text-white">
+                                Type<br/><br/>
+                            </div>
+                            <?php echo form_dropdown('type', $options, 'required class="form-control"');?>
+                        </div>
+                        <?php 
+                            echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');
+                            echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');
+                            echo form_close();
+                        ?>
                     </div>
                 </div>
             </div>
--- application/views/form_modify_user.php
+++ application/views/form_modify_user.php
@@ -14,6 +14,19 @@
                             <?php echo form_input('name',$user['name'],'required class="form-control"')?>
                             <br/><br/>
                         </div>
+                        <div class="form-group">
+                            <div class="text-white">
+                                Password<br/><br/>
+                            </div>
+                            <?php echo form_password('password',$user['password'],'required class="form-control"')?>
+                        </div>
+                        <?php $options = array('1' => 'Admin','2' => 'Officer','3' => 'Member');?>
+                        <div class="form-group">
+                            <div class="text-white">
+                                Type<br/><br/>
+                            </div>
+                            <?php echo form_dropdown('type', $options, 'required class="form-control"');?>
+                        </div>
                         <?php echo form_hidden('id',$user['id']);?>
                         <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
                         <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
