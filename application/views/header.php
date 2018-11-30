9ff25942ddc118c9c40a47e269706ae4f369a189
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 10:31:00 +0100

    added bootstrap styles, redesigned and implemented bootstrap, added logo

--- application/views/header.php
+++ application/views/header.php
@@ -3,8 +3,15 @@
     <head>
         <meta charset="UTF-8"/>
         <title>Tight Underpoints</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets\css\style.css" title="style"/>
         <script type="text/javascript" src="<?php echo base_url()?>assets\js\scripts.js"></script>
     </head>
 
-    <body>
+    <body class="bg-dark">
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 offset-4">
+                <img src="<?php echo base_url()?>\assets\img\logo.PNG" class="rounded" alt="Responsive Logo">
+            </div>
+        </div>
