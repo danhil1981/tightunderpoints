138de4db4574f993cf7d93b41ef0321340061d53
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-11-30 19:31:51 +0100

    removed obsolete css link

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8"/>
         <title>Tight Underpoints</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets\css\style.css" title="style"/>
         <script type="text/javascript" src="<?php echo base_url()?>assets\js\scripts.js"></script>
     </head>
 
