6d43bcc3dbf572d504991a6766403b1cc0d2bc25
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-02 14:14:11 +0100

    modified design, added character table

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -2,7 +2,7 @@
 <br/><br/>
 
 <div class="row">
-    <div class="col-10 offset-1">
+    <div class="col-10 offset-1 text-center">
         <?php if(isset($msg)) echo $msg ?>
         <br/><br/>
         <button onclick='show("users")' id='button_users' class="btn btn-primary">Users</button>
@@ -24,21 +24,21 @@
 <div class="row">
     <div class="col-8 offset-2">
         <div id='users'>
-            <table class='table-dark'>
+            <table class='table table-dark table-striped table-bordered table-hover table-sm text-center'>
                 <thead>
                     <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>&nbsp;</th>
-                        <th>&nbsp;</th>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">&nbsp;</th>
+                        <th scope="col">&nbsp;</th>
                     </tr>
                 </thead>
                 <?php
                     for ($i = 0; $i < count($users_list); $i++) {
                         $user = $users_list[$i];
                 
-                        echo "<tr><td>".$user['id']."</td>";
+                        echo "<tr><th scope='row'>".$user['id']."</th>";
                         echo "<td>".$user['name']."</td>";
                         echo "<td>".$user['type']."</td>";
                         echo "<td><a href='".site_url()."/users/delete/".$user['id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
@@ -52,20 +52,20 @@
         </div>
 
         <div id='players' style='display: none;'>
-            <table class='table-dark'>
+            <table class='table table-dark table-striped table-bordered table-hover table-sm text-center'>
                 <thead>
                     <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>&nbsp;</th>
-                        <th>&nbsp;</th>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">&nbsp;</th>
+                        <th scope="col">&nbsp;</th>
                     </tr>
                 </thead>
                 <?php
                     for ($i = 0; $i < count($players_list); $i++) {
                         $player = $players_list[$i];
                 
-                        echo "<tr><td>".$player['id']."</td>";
+                        echo "<tr><th scope='row'>".$player['id']."</th>";
                         echo "<td>".$player['name']."</td>";
                         echo "<td><a href='".site_url()."/players/delete/".$player['id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
                         echo "<td><a href='".site_url()."/players/show_modify/".$player['id']."' class='btn btn-warning btn-sm''>Modify</a></td></tr>";
@@ -74,6 +74,40 @@
             </table>
             <br/>
             <a href='<?php echo site_url()?>/players/show_insert/' class='btn btn-success btn-sm'>New</a>
+            <br/><br/>
+        </div>
+
+        <div id='characters' style='display: none;'>
+            <table class='table table-dark table-striped table-bordered table-hover table-sm text-center'>
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Level</th>
+                        <th scope="col">Class</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Player</th>
+                        <th scope="col">&nbsp;</th>
+                        <th scope="col">&nbsp;</th>
+                    </tr>
+                </thead>
+                <?php
+                    for ($i = 0; $i < count($characters_list); $i++) {
+                        $character = $characters_list[$i];
+                
+                        echo "<tr><th scope='row'>".$character['id']."</th>";
+                        echo "<td>".$character['name']."</td>";
+                        echo "<td>".$character['level']."</td>";
+                        echo "<td>".$character['class']."</td>";
+                        echo "<td>".$character['type']."</td>";
+                        echo "<td>".$character['name_player']."</td>";
+                        echo "<td><a href='".site_url()."/characters/delete/".$character['id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
+                        echo "<td><a href='".site_url()."/characters/show_modify/".$character['id']."' class='btn btn-warning btn-sm'>Modify</a></td></tr>";
+                    }
+                ?>
+            </table>
+            <br/>
+            <a href='<?php echo site_url()?>/characters/show_insert/' class='btn btn-success btn-sm'>New</a>
             <br/><br/>
         </div>
     </div>
