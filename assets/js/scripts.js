e6d7f7f064b2d6aafed129e8060dbe8a858dd63a
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-03 12:46:23 +0100

    Added datatables jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,15 @@
 tables = ['users','players','characters'/*,'raids','events','bosses','items','drops','attendence','loots'*/];
 
+
+$(document).ready(function () {
+    for (i = 0; i < tables.length; i++) {
+        $('#table_'+tables[i]).DataTable();
+    }
+});
+
 function show(table) {
     for (i=0; i<tables.length;i++) {
-        document.getElementById("button_"+tables[i]).className = "btn";
+        document.getElementById("button_" + tables[i]).className = "btn btn-light";
         document.getElementById(tables[i]).style.display = "none";
     }
     document.getElementById("button_"+table).className = "btn btn-primary";
