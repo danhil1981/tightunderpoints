4d1eab4e3fb811745a200c963ea08ffde19aacd7
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-03 18:31:18 +0100

    added raids mvc

---
+++ application/models/Model_raids.php
@@ -0,0 +1,66 @@
+<?php
+
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Model_Raids extends CI_Model {
+
+        public function get_all() {
+            $query = $this->db->query("SELECT * FROM raids;");
+            $raids = array();
+            if ($query->num_rows() > 0) {
+                foreach ($query->result_array() as $row) {
+                    $raids[] = $row;
+                }
+            }
+            return $raids;
+        }
+    
+        public function get($id) {
+            $query = $this->db->query("SELECT * FROM raids WHERE id = $id;");
+            $raid = $query->result_array()[0];
+    
+            return $raid;
+        }
+    
+        public function delete($id) {
+    
+            $this->db->query("DELETE FROM raids WHERE id = $id ;");
+    
+            return $this->db->affected_rows();            
+        }
+    
+    
+        public function insert() {
+            $description = $this->input->post('description');
+            $date = $this->input->post("date");
+            
+    
+            $this->db->query("INSERT INTO raids (description, date ) VALUES ('$description', '$date');");
+    
+            return $this->db->affected_rows(); 
+        }
+    
+        public function modify() {
+            $id = $this->input->post('id');
+            $description = $this->input->post('description');
+            $date = $this->input->post('date');
+    
+            $this->db->query("UPDATE raids SET description = '$description', date = '$date' WHERE id = $id;");
+    
+            return $this->db->affected_rows();
+        }
+
+        public function get_list() {
+            $query = $this->db->query("SELECT id AS id_raid, date AS date_raid FROM raids;");
+            $raids = array();
+            if ($query->num_rows() > 0) {
+                foreach ($query->result_array() as $row) {
+                    $raids[] = $row;
+                }
+            }
+            return array_column($raids, 'date_raid', 'id_raid');
+        }
+    }
+
+?>
---
+++ application/views/form_modify_raid.php
@@ -0,0 +1,29 @@
+            <div class="row">
+                            <div class="col-6 offset-3">
+                                <h1 class="text-center text-white">Modify Raid</h1>
+
+                                <br/><br/>
+                    
+                                <?php echo form_open_multipart('raids/modify');?>
+
+                                <div class="form-group">
+                                    <div class="text-white">
+                                        Description<br/><br/>
+                                    </div>
+                                    <?php echo form_input('description',$raid['description'],'required class="form-control"')?>
+                                </div>
+                                <div class="form-group">
+                                    <div class="text-white">
+                                        Date<br/><br/>
+                                    </div>
+                                    <?php echo form_date('date', $raid['date'], 'required class="form-control"')?>
+                                </div>
+                                
+                                <?php 
+                                    echo form_hidden('id',$raid['id']);
+                                    echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');
+                                    echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');
+                                    echo form_close();
+                                ?>
+                            </div>
+                        </div>
