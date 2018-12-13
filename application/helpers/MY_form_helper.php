daad93adc5dd1cbea3dfc6bd4eb36e79b31d05b5
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-13 13:57:50 +0100

    refactoring

--- application/helpers/MY_form_helper.php
+++ application/helpers/MY_form_helper.php
@@ -4,14 +4,22 @@
 
     if ( ! function_exists('form_date')) {
         function form_date($data = "", $value = "", $extra = "") {
-            $defaults = array('type' => 'date', 'name' => (( ! is_array($data)) ? $data : ""), 'value' => $value);
+            $defaults = array(
+                'type' => 'date',
+                'name' => (( ! is_array($data)) ? $data : ""),
+                'value' => $value
+            );
             return "<input "._parse_form_attributes($data, $defaults).$extra." />";
         }
     }
 
     if ( ! function_exists('form_number')) {
         function form_number($data = "", $value = "", $extra = "") {
-            $defaults = array('type' => 'number', 'name' => (( ! is_array($data)) ? $data : ""), 'value' => $value);
+            $defaults = array(
+                'type' => 'number',
+                'name' => (( ! is_array($data)) ? $data : ""),
+                'value' => $value
+            );
             return "<input "._parse_form_attributes($data, $defaults).$extra." />";
         }
     }
