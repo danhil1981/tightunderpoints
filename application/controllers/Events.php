838b73be032c1535947acc3a1d89d5708263d8bf
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-17 13:16:43 +0100

    created events controller

---
+++ application/controllers/Events.php
@@ -0,0 +1,123 @@
+<?php
+    include('Security.php');
+
+    Class Events extends Security {
+
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('model_players');
+            $this->load->model('model_users');
+            $this->load->model('model_characters');
+            $this->load->model('model_bosses');
+            $this->load->model('model_raids');
+            $this->load->model('model_items');
+            $this->load->model('model_events');
+        }
+
+        public function load_view($data = NULL) {
+            $data['users_list'] = $this->model_users->get_all();
+            $data['players_list'] = $this->model_players->get_all();
+            $data['characters_list'] = $this->model_characters->get_all();
+            $data['bosses_list'] = $this->model_bosses->get_all();
+            $data['raids_list'] = $this->model_raids->get_all();
+            $data['items_list'] = $this->model_items->get_all();
+            $data['events_list'] = $this->model_events->get_all();
+            $data['view_name'] = 'admin_panel';
+            $data["table_to_show"] = "events";
+            $this->load->view('template', $data);
+        }
+
+        
+        public function show_insert() {
+
+            if ($this->check_login()) {
+    
+                $data['view_name'] = 'form_insert_event';
+                $data['raid_descriptions'] = $this->model_raids->get_list_latest_first();
+                $data['boss_names'] = $this->model_bosses->get_list();
+                $this->load->view('template', $data);
+    
+            }
+        }
+
+        public function insert() {
+
+            if ($this->check_login()) {
+    
+                    $result_insert = $this->model_events->insert();
+    
+                    if ($result_insert == 0) {
+    
+                        $data['msg'] = "<div class='badge badge-danger'>Error on insertion</div><br/>";
+                    
+                    }
+    
+                    else {
+    
+                        $data['msg'] = "<div class='badge badge-success'>Event successfully inserted</div><br/>";
+    
+                    }    
+    
+                $this->load_view($data);
+    
+            }
+        }
+
+        public function delete($id) {
+
+            if ($this->check_login()) {
+    
+                $result = $this->model_events->delete($id);
+    
+                if ($result == 0) {
+    
+                    $data['msg'] = "<div class='badge badge-danger'>Error on deletion</div><br/>";
+    
+                }
+                
+                else {
+    
+                    $data['msg'] = "<div class='badge badge-success'>Event successfully deleted</div><br/>";
+                
+                }
+    
+               $this->load_view($data); 
+    
+            }
+        }
+
+        public function show_modify($id) {
+
+            if ($this->check_login()) {
+    
+                $data['view_name'] = 'form_modify_event';
+                $data['event'] = $this->model_events->get($id);
+                $data['raid_descriptions'] = $this->model_raids->get_list();
+                $data['boss_names'] = $this->model_bosses->get_list();
+                $this->load->view('template', $data);
+    
+            }
+        }
+
+        public function modify() {
+
+            if ($this->check_login()) {
+    
+                $result = $this->model_events->modify();
+    
+                if ($result == 0) {
+    
+                    $data['msg'] = "<div class='badge badge-danger'>Error on modification</div><br/>";
+    
+                }
+                else {
+    
+                    $data['msg'] = "<div class='badge badge-success'>Event successfully modified</div><br/>";
+    
+                }
+                
+                $this->load_view($data); 
+            }
+        }
+    }
+?>
