a544095b8394527371bdb6edcb5ba67e3772da37
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-17 13:31:50 +0100

    merge fix

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,7 +4,6 @@
     Class Users extends Security {
 
         public function load_view($data = NULL) {
-<<<<<<< HEAD
             $data['users_list'] = $this->model_users->get_all();
             $data['players_list'] = $this->model_players->get_all();
             $data['characters_list'] = $this->model_characters->get_all();
@@ -13,8 +12,6 @@
             $data['items_list'] = $this->model_items->get_all();
             $data['events_list'] = $this->model_events->get_all();
             $data['view_name'] = "admin_panel";
-=======
->>>>>>> 63be1d54bc31d4e7761077c5cd8efb7580057357
             $data["table_to_show"] = "users";
             $this->load->view('template', $data);
         }
