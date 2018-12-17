cb37fd9fe640d94dc898b06f985e88594135b294
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-17 13:43:47 +0100

    changed date to gmdate for more accurate date display

--- application/views/form_insert_raid.php
+++ application/views/form_insert_raid.php
@@ -1,28 +1,28 @@
             <div class="row">
-                            <div class="col-6 offset-3">
-                                <h1 class="text-center text-white">New Raid</h1>
+                <div class="col-6 offset-3">
+                    <h1 class="text-center text-white">New Raid</h1>
 
-                                <br/><br/>
-                    
-                                <?php echo form_open_multipart('raids/insert');?>
+                    <br/><br/>
+        
+                    <?php echo form_open_multipart('raids/insert');?>
 
-                                <div class="form-group">
-                                    <div class="text-white">
-                                        Description<br/><br/>
-                                    </div>
-                                    <?php echo form_input('description','','required class="form-control"')?>
-                                </div>
-                                <div class="form-group">
-                                    <div class="text-white">
-                                        Date<br/><br/>
-                                    </div>
-                                    <?php echo form_date('date',date('Y-m-d'), 'required class="form-control"')?>
-                                </div>
-                                
-                                <?php 
-                                    echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');
-                                    echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');
-                                    echo form_close();
-                                ?>
-                            </div>
+                    <div class="form-group">
+                        <div class="text-white">
+                            Description<br/><br/>
                         </div>
+                        <?php echo form_input('description','','required class="form-control"')?>
+                    </div>
+                    <div class="form-group">
+                        <div class="text-white">
+                            Date<br/><br/>
+                        </div>
+                        <?php echo form_date('date',gmdate('Y-m-d'), 'required class="form-control"')?>
+                    </div>
+                    
+                    <?php 
+                        echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');
+                        echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');
+                        echo form_close();
+                    ?>
+                </div>
+            </div>
