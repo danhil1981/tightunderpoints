1a537d64b6b8425922dd18e33c113a986b06d7b6
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-17 12:49:52 +0100

    created controller for events

---
+++ application/controllers/Events.php
@@ -0,0 +1,105 @@
+<?php
+    include('Security.php');
+
+    Class Events extends Security {
+
+        public function load_view($data = NULL) {
+            $data['view_name'] = 'admin_panel';
+            $data["table_to_show"] = "events";
+            $this->load->view('template', $data);
+        }
+
+        
+        public function show_insert() {
+
+            if ($this->check_login()) {
+    
+                $data['view_name'] = 'form_insert_event';
+                $data['raid_descriptions'] = $this->model_raids->get_list_latest_first();
+                $data['boss_names'] = $this->model_bosses->get_list();
+                $this->load->view('template', $data);
+    
+            }
+        }
+
+        public function insert() {
+
+            if ($this->check_login()) {
+    
+                    $result_insert = $this->model_events->insert();
+    
+                    if ($result_insert == 0) {
+    
+                        $data['msg'] = "<div class='badge badge-danger'>Error on insertion</div><br/>";
+                    
+                    }
+    
+                    else {
+    
+                        $data['msg'] = "<div class='badge badge-success'>Event successfully inserted</div><br/>";
+    
+                    }    
+    
+                $this->load_view($data);
+    
+            }
+        }
+
+        public function delete($id) {
+
+            if ($this->check_login()) {
+    
+                $result = $this->model_events->delete($id);
+    
+                if ($result == 0) {
+    
+                    $data['msg'] = "<div class='badge badge-danger'>Error on deletion</div><br/>";
+    
+                }
+                
+                else {
+    
+                    $data['msg'] = "<div class='badge badge-success'>Event successfully deleted</div><br/>";
+                
+                }
+    
+               $this->load_view($data); 
+    
+            }
+        }
+
+        public function show_modify($id) {
+
+            if ($this->check_login()) {
+    
+                $data['view_name'] = 'form_modify_event';
+                $data['event'] = $this->model_events->get($id);
+                $data['raid_descriptions'] = $this->model_raids->get_list();
+                $data['boss_names'] = $this->model_bosses->get_list();
+                $this->load->view('template', $data);
+    
+            }
+        }
+
+        public function modify() {
+
+            if ($this->check_login()) {
+    
+                $result = $this->model_events->modify();
+    
+                if ($result == 0) {
+    
+                    $data['msg'] = "<div class='badge badge-danger'>Error on modification</div><br/>";
+    
+                }
+                else {
+    
+                    $data['msg'] = "<div class='badge badge-success'>Event successfully modified</div><br/>";
+    
+                }
+                
+                $this->load_view($data); 
+            }
+        }
+    }
+?>
