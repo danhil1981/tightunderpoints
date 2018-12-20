a8ae18ab72752d9d38637cf217d8410f2db12709
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-20 10:52:38 +0100

    added drops table

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -252,6 +252,36 @@
                         <br/><br/>
                     </div>
 
+                     <div id='drops' style='display: none;'>
+                        <table class='table table-dark table-striped table-bordered table-hover table-sm text-center' id="table_drops">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Event</th>
+                                    <th scope="col">Item</th>
+                                    <th scope="col">&nbsp;</th>
+                                    <th scope="col">&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    for ($i = 0; $i < count($drops_list); $i++) {
+                                        $drop = $drops_list[$i];
+                                
+                                        echo "<tr><th scope='row'>".$drop['id']."</th>";
+                                        echo "<td>".$drop['name_event']."</td>";
+                                        echo "<td>".$drop['name_item']."</td>";
+                                        echo "<td><a href='".site_url()."/drops/delete/".$drop['id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
+                                        echo "<td><a href='".site_url()."/drops/show_modify/".$drop['id']."' class='btn btn-warning btn-sm'>Modify</a></td></tr>";
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                        <br/>
+                        <a href='<?php echo site_url()?>/drops/show_insert/' class='btn btn-success btn-sm'>New</a>
+                        <br/><br/>
+                    </div>
+
                 </div>
             </div>
 
