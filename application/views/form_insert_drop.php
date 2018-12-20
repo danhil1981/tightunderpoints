60d5d13dbe4702353f8be8f99fb066675d981daf
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-20 10:52:51 +0100

    added drops forms

---
+++ application/views/form_insert_drop.php
@@ -0,0 +1,31 @@
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <h1 class="text-center text-white">New Drop</h1>
+
+                    <br/><br/>
+        
+                    <?php echo form_open_multipart('drops/insert');?>
+
+                    <?php $options = $event_names;?>
+                    <div class="form-group">
+                        <div class="text-white">
+                            Event<br/><br/>
+                        </div>
+                        <?php echo form_dropdown('id_event', $options, '', 'required class="form-control"');?>
+                        <br/><br/>
+                    </div>
+
+                    <?php $options = $item_names;?>
+                    <div class="form-group">
+                        <div class="text-white">
+                            Item<br/><br/>
+                        </div>
+                        <?php echo form_dropdown('id_item', $options, '', 'required class="form-control"');?>
+                        <br/><br/>
+                    </div>
+
+                    <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-small"');?>
+                    <?php echo anchor('users/admin_panel', 'Cancel', 'class="btn btn-danger btn-small"');?>
+                    <?php echo form_close();?>
+                </div>
+            </div>
