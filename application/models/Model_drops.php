7c340003d4210a8133a7aa6d4593ac77df89edb4
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2018-12-20 10:51:44 +0100

    created drops model

---
+++ application/models/Model_drops.php
@@ -0,0 +1,54 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Model_Drops extends CI_Model {
+
+        public function get_all() {
+            $query = $this->db->query("SELECT drops.id, drops.id_event, drops.id_item, CONCAT(events.timestamp,' - ', bosses.name) AS name_event, items.name AS name_item FROM drops INNER JOIN events ON drops.id_event = events.id INNER JOIN items ON drops.id_item = items.id INNER JOIN bosses ON events.id_boss = bosses.id ;");
+            $drops = array();
+            if ($query->num_rows() > 0) {
+                foreach ($query->result_array() as $row) {
+                    $drops[] = $row;
+                }
+            }
+            return $drops;
+        }
+    
+        public function get($id) {
+            $query = $this->db->query("SELECT * FROM drops WHERE id = $id;");
+            $drop = $query->result_array()[0];
+    
+            return $drop;
+        }
+    
+        public function delete($id) {
+    
+            $this->db->query("DELETE FROM drops WHERE id = $id ;");
+    
+            return $this->db->affected_rows();            
+        }
+    
+    
+        public function insert() {
+            $id_event = $this->input->post('id_event');
+            $id_item = $this->input->post('id_item');
+    
+            $this->db->query("INSERT INTO drops (id_event, id_item) VALUES ('$id_event', '$id_item');");
+    
+            return $this->db->affected_rows(); 
+        }
+    
+        public function modify() {
+            $id = $this->input->post('id');
+            $id_event = $this->input->post('id_event');
+            $id_item = $this->input->post('id_item');
+    
+            $this->db->query("UPDATE drops SET id_event = '$id_event', id_item = '$id_item' WHERE id = $id;");
+    
+            return $this->db->affected_rows();
+        }
+
+    }
+
+?>
