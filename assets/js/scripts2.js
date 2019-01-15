3741d67d16dac0ceafcdd29ae905f289286dce31
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-15 14:27:12 +0100

    added new script for officer panel

---
+++ assets/js/scripts2.js
@@ -0,0 +1,17 @@
+tables = ['points'/*,'timer'*/];
+
+
+$(document).ready(function () {
+    for (i = 0; i < tables.length; i++) {
+        $('#table_' + tables[i]).DataTable({"order": [[1, "asc"]]});
+    }
+});
+
+function show(table) {
+    for (i=0; i<tables.length;i++) {
+        document.getElementById("button_" + tables[i]).className = "btn btn-light btn-sm";
+        document.getElementById(tables[i]).style.display = "none";
+    }
+    document.getElementById("button_"+table).className = "btn btn-primary btn-sm";
+    document.getElementById(table).style.display = "block";
+}
