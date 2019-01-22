032861865e8a66650c49dbef66631cc4a4e952fb
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-22 08:24:49 +0100

    Added character type column to tables

--- application/views/officer_panel.php
+++ application/views/officer_panel.php
@@ -1,7 +1,7 @@
             <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/custom.css">
             <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
             <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-            <script src="<?php echo base_url()?>assets/js/scripts2.js"></script>
+            <script src="<?php echo base_url()?>assets/js/officer_panel.js"></script>
             
             <h1 class='text-center text-white'>Officer Panel</h1>
 
@@ -22,6 +22,7 @@
                             <thead>
                                 <tr>
                                     <th scope="col">Name</th>
+                                    <th scope="col">Type</th>
                                     <th scope="col">Available Points</th>
                                     <th scope="col">&nbsp;</th>
                                 </tr>
@@ -29,15 +30,28 @@
                             <tbody id="compare_tbody">
                             </tbody>
                         </table>
-                        <br/><br/>
+                        <br/>
                     </div>
                 </div>
+
+                <div class="col-4 offset-4">
+                        <div id="winner" style="display: none">
+                            <table class='table table-dark table-striped table-bordered table-hover table-sm text-center' id="table_winner">
+                                <tbody id="winner_tbody">
+                                </tbody>
+                            </table>
+                        </div>
+                        <br/>
+                    </div>
+                </div>
+    
                 <div class="col-8 offset-2" id="tables">
                     <div id='points'>
                         <table class='table table-dark table-striped table-bordered table-hover table-sm text-center' id="datatable_points">
                             <thead>
                                 <tr>
                                     <th scope="col">Name</th>
+                                    <th scope="col">Type</th>
                                     <th scope="col">All Time Earned</th>
                                     <th scope="col">All Time Spent</th>
                                     <th scope="col">Earned last 50 days</th>
@@ -48,14 +62,15 @@
                             </thead>
                             <tbody>
                                 <?php
-                                    foreach ($character_names as $i => $value) {
+                                    foreach ($list_names as $i => $value) {
                                         echo "<tr><td id='name_$i'>".$value."</td>";
+                                        echo "<td id='type_$i'>".$list_types[$i]."</td>";
                                         echo "<td>".$list_total_earned[$i]."</td>";
                                         echo "<td>".$list_total_spent[$i]."</td>";
                                         echo "<td>".$list_last50_earned[$i]."</td>";
                                         echo "<td>".$list_last50_spent[$i]."</td>";
                                         echo "<td id='points_$i'>".($list_last50_earned[$i]-$list_last50_spent[$i])."</td>";
-                                        echo "<td><button class='btn btn-sm btn-warning' id='compare_".$i."'>Compare</button></td></tr>";
+                                        echo "<td><button class='btn btn-sm btn-info' id='compare_".$i."'>Compare</button></td></tr>";
                                     }
                                 ?>
                             </tbody>
