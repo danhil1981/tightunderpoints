d49193c149df9a788096c9bcf30bb681c6e0582d
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-23 14:05:27 +0100

    Rewrote the logic for selection of characters

--- assets/js/officer_panel.js
+++ assets/js/officer_panel.js
@@ -1,58 +1,27 @@
-$(document).ready(function () {
-    $("[id^='datatable']").DataTable({"order": [[0, "asc"]]});
+var comparing = [];
 
-    var max_points = Number.MIN_SAFE_INTEGER;
-    var max_id = "";
-    var max_type = "Bot";
-    var multiples = [];
-    var winner_id = "";
-    var comparing = [];
+$(document).ready(function () {
+    $("[id^='datatable']").DataTable();
 
     $('#points').on('click', "button", function () {
         $("#compare").css("display", "block");
         $("#winner").css("display", "block");
-        var id = (this.id.slice(8));
-        var name = $("#name_" + id).html();
-        var points = $("#points_" + id).html();
-        var type = $("#type_" + id).html();
-        comparing.push(id,name,points,type);
-        for (var i = 0; i < (comparing/4).length(); i+4) {
+        var id = this.id.slice(8);
+        var points = parseInt($("#points_" + id).html());
+        var type = parseInt($("#type_" + id).html());
 
-        }
-        /*$("#compare_tbody").append("<tr id='row_" + comparing + "'><td>" + $("#name_" + id).html() + "</td><td>" + $("#type_" + id).html() + "</td><td>" + $("#points_" + id).html()
+        comparing.push(id,points,type);
+        $("#compare_tbody").append("<tr id='row_" + id + "'><td>" + $("#name_" + id).html() + "</td><td>" + $("#type_" + id).html() + "</td><td>" + $("#points_" + id).html()
             + "</td><td><button class='btn btn-sm btn-info' id='remove_" + id + "'>Remove</button></td></tr>");
-
-        if (parseInt($("#points_" + id).html()) > max_points) {
-            multiples = [];
-            max_points = parseInt($("#points_" + id).html()); 
-            max_id = $("#name_" + id).html();
-            $("#winner_tbody").html("<tr><td>"+max_id+"</td><td>"+max_points+"</td></tr>");
-        }
-        if (parseInt($("#points_" + id).html()) == max_points) {
-            multiples.push(id);
-            winner_id = multiples[Math.floor(Math.random() * multiples.length)];
-            $("#winner_tbody").html("<tr><td>" + $("#name_"+winner_id).html() + "</td><td>" + max_points + "</td></tr>");
-        } */
+        get_winner();
     });
 
     $('#compare').on('click', "button", function () {
         var id = (this.id.slice(7));
         $('#row_'+id).remove();
-        if (parseInt($("#points_" + id).html()) < max_points) {
-            $("#winner_tbody").html("<tr><td>" + $("#name_" + id).html() + "</td><td>" + $("#points_" + id).html() + "</td></tr>");
-        }
-        else {
-            multiples.slice(id,1);
-            winner_id = multiples[Math.floor(Math.random() * multiples.length)];
-            $("#winner_tbody").html("<tr><td>" + $("#name_" + winner_id).html() + "</td><td>" + max_points + "</td></tr>");
-        }
-        winner_id = multiples[Math.floor(Math.random() * multiples.length)];
-        $("#winner_tbody").html("<tr><td>" + $("#name_" + winner_id).html() + "</td><td>" + max_points + "</td></tr>");
-        if ($('#compare tr').length <= 1) {
-            $('#compare').css("display", "none");
-            $("#winner_tbody").html("");
-            $('#winner').css("display", "none");
-        }
+        var index = comparing.indexOf(id);
+        comparing.splice(index,3);
+        get_winner();
     });
 
     $('#menu_buttons').on('click', "button", function () {
@@ -65,3 +34,33 @@ $(document).ready(function () {
     });
 
 });
+
+function get_winner() {
+    var max_id = "";
+    var max_points = Number.MIN_SAFE_INTEGER;
+    var max_type = 3;
+    var multiples = [];
+    for (var i = 0; i < comparing.length; i=i+3) {
+        if (comparing[i + 2] < max_type) {
+            max_id = comparing[i];
+            max_points = comparing[i + 1];
+            max_type = comparing[i + 2];
+        }
+        else {
+            if (comparing[i + 2] == max_type) {
+                if (comparing[i + 1] > max_points) {
+                    var multiples = []
+                    max_id = comparing[i];
+                    max_points = comparing[i + 1];
+                }
+                 
+                if (comparing[i + 1] == max_points) {
+                    multiples.push(max_id);
+                    multiples.push(comparing[i]);
+                    max_id = multiples[Math.floor(Math.random() * multiples.length)];
+                }
+            }
+        }
+    }
+    $("#winner_tbody").html("<tr><td>" + $("#name_" + max_id).html() + "</td><td>" + $("#points_" + max_id).html() + "</td><td><a href='officers/loot/"+max_id+"' class='btn btn-sm btn-success'>Loot</a></td></tr>");
+}
