e0a03504b3074ee182ed48ca916711212b9c39fd
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-25 14:40:37 +0100

    added ajax functionality

--- application/views/form_insert_loot_entry_officers.php
+++ application/views/form_insert_loot_entry_officers.php
@@ -1,20 +1,46 @@
             <script>
                 $(document).ready( function () {
                     $("#insert_raid").click( function() {
-                        var description1 = $("#raid_description").val();
-                        var date1 = $("#raid_date").val();
-                        
-                        $.ajax({ url: 'officers/insert_raid',
-                            data: { description: description1, date: date1 },
+                        var description = $("#raid_description").val();
+                        var date = $("#raid_date").val();
+            
+                        $.ajax({ 
+                            url: '<?php echo site_url()?>/officers/insert_raid/',
+                            data: { 'description':description, 'date':date },
                             type: 'post',
-                        }).done(function(output) {
-                                if (output == 0) {
-                                    $("body").append('<div class="alert alert-success" role="alert">Success</div>');
+                            success: function(output) {
+                                if (parseInt(output) < 1) {
+                                    $("#messages").html("<br><br><div class='badge badge-success'>Error on insertion</div><br/>");
                                 }
                                 else {
-                                    $("body").append('<div class="alert alert-danger" role="alert">Fail</div>');
+                                    $("#messages").html("<br><br><div class='badge badge-success'>Raid successfully inserted</div><br/>");
+                                    var id_raid = output;
+                                    var description_raid = date +" - " +description;
+                                    $("#raid_dropdown").append(new Option(description_raid, id_raid));
+                                    $('#raid_dropdown option:last').attr("selected", "selected");
                                 }
+                            }
                         });
+                        $("#modal_raid").removeClass("show");
+                        $("body").removeClass("modal-open");
+                        $(".modal-backdrop").remove();
+                        $('#form_raid')[0].reset();
+                        return false;
+                    });
+                    
+                    $("#raid_dropdown").change(function() {
+                        var id_raid = $("#raid_dropdown").prop('selectedIndex');
+                        $.ajax({
+                            url: '<?php echo site_url()?>/officers/get_events/',
+                            data: {'id_raid':id_raid},
+                            type: 'post', 
+                            success: function(output) {
+                                $.each(output, function(test1, test2) {
+                                    $("#event_dropdown").append($('<option></option>').attr('value', test1).text(test2));
+                                });   
+                            }
+                        });
+                        return false;
                     });
                 });
             </script>
@@ -31,7 +57,7 @@
                         <div class="modal-body">
                             <h1 class="text-center text-white">New Raid</h1>
             
-                            <form>
+                            <form id="form_raid">
                                 Description:
                                 <input type="text" id="raid_description" class="form-control"/>
                                 <br/>Date:
@@ -45,13 +71,35 @@
                 </div>
             </div>
 
+            <div class="modal fade" id="modal_event" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">New Raid</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <h1 class="text-center text-white">New Event</h1>
+            
+                            <form id="form_raid">
+                                FORM HERE
+                                <input type="submit" value="Submit" id="insert_event" class="btn btn-primary"/>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="row" id="content">
                 <div class="col-6 offset-3">
                     <h1 class="text-center text-white">New Loot Entry</h1>
-
+                    <div id="messages" class="text-center"></div>
                     <br/><br/>
         
-                    <?php echo form_open_multipart('officers/insert_loot');?>
+                    <?php echo form_open_multipart('');?>
 
                     <div class="form-group">
                         <div class="text-white">
@@ -66,21 +114,19 @@
                             Raid<br/><br/>
                         </div>
                         <div class="form-inline d-block">
-                            <?php $options = $raid_descriptions;array_unshift($options,"-- Not part of a raid --");echo form_dropdown('id_raid', $options , '', 'class="form-control float-left"');?>
-                            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal_raid">
-                                New
-                            </button>
+                            <?php $options = $raid_descriptions;array_unshift($options,"-- Not part of a raid --");echo form_dropdown('id_raid', $options , '', 'id="raid_dropdown" class="form-control float-left"');?>
+                            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal_raid">New</button>
                         </div>
                         <br/><br/>
                     </div>
 
-                    <div class="form-group d-none" id="event_dropdown">
+                    <div class="form-group" id="event_dropdown">
                         <div class="text-white">
                             Event<br/><br/>
                         </div>
                         <div class="form-inline d-block">
-                            <?php echo form_dropdown('id_event', '' , '', 'class="form-control float-left"');?>
-                            <button class='btn btn-primary float-right' onclick="new_event();">New</button>
+                            <?php echo form_dropdown('id_event', $events_not_raid , '', 'class="form-control float-left"');?>
+                            <button class='btn btn-primary float-right' data-toggle="modal" data-target="#modal_event";">New</button>
                         </div>
                         <br/><br/>
                     </div>
