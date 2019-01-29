cd46261ae6a7abb693c7f73e25e42ae0aac71d07
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-29 14:28:57 +0100

    removed obsolete method

--- application/controllers/Officers.php
+++ application/controllers/Officers.php
@@ -16,22 +16,6 @@
 
         }
 
-        public function point_list() {
-
-            if ($this->check_login()) {
-                $data['list_names'] = $this->model_characters->get_list_names();
-                $data['list_types'] = $this->model_characters->get_list_types();
-                $data['list_total_earned'] = $this->model_characters->get_list_total_earned();
-                $data['list_last50_earned'] = $this->model_characters->get_list_last50_earned();
-                $data['list_total_spent'] = $this->model_characters->get_list_total_spent();
-                $data['list_last50_spent'] = $this->model_characters->get_list_last50_spent();
-                $data['view_name'] = 'officer_panel';
-                $data["table_to_show"] = "point_list";
-                $this->load_view($data);
-            }
-
-        }
-
         public function loot($id) {
             if ($this->check_login()) {    
                 $data['view_name'] = 'form_insert_loot_entry_officers';
