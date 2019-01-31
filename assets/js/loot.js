b963b4e660a3202bd39317fed7a172486f847333
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-31 20:27:14 +0100

    now triggering the change event on the correct dropdown

--- assets/js/loot.js
+++ assets/js/loot.js
@@ -1,5 +1,6 @@
 $(document).ready( function () {
     path = window.location.href.substring(0,window.location.href.lastIndexOf('loot'));
+
     $("#insert_raid").click( function() {
         var description = $("#raid_description").val();
         var date = $("#raid_date").val();
@@ -81,7 +82,7 @@ $(document).ready( function () {
         $("body").removeClass("modal-open");
         $(".modal-backdrop").remove();
         $('#form_event')[0].reset();
-        $("#event_dropdown").trigger();
+        $("#raid_dropdown").trigger("change");
         return false;
     });
 
@@ -100,6 +101,45 @@ $(document).ready( function () {
         });
         return false;
     });
+
+    $("#new_item").click(function() {
+        var id_event = $("#event_dropdown").val();
+        $.ajax({ 
+            url: path +'get_boss/',
+            data: { 'id_event':id_event},
+            type: 'post',
+            success: function(output) {
+                $('#boss_dropdown').val(output);
+                }
+        });
+    });
+
+    $("#insert_item").click( function() {
+        var id_item = $("#id_item").val();
+        var name_item = $("#name_item").val();
+        var id_boss = $("#boss_dropdown").val();
+        var value_item = $("#value_item").val();
+        $.ajax({ 
+            url: path +'insert_item/',
+            data: { 'id_item':id_item, 'name_item':name_item, 'id_boss':id_boss , 'value_item':value_item  },
+            type: 'post',
+            success: function(output) {
+                if (parseInt(output) == 0) {
+                    $("#messages").html("<br><br><div class='badge badge-success'>Error on insertion</div><br/>");
+                }
+                else {
+                    $("#messages").html("<br><br><div class='badge badge-success'>Item successfully inserted</div><br/>");
+                }
+            }
+        });
+        $("#modal_item").removeClass("show");
+        $("body").removeClass("modal-open");
+        $(".modal-backdrop").remove();
+        $('#form_item')[0].reset();
+        $("#raid_dropdown").trigger("change");
+        return false;
+    });
+    $("#raid_dropdown").trigger("change");
 
 });
 
