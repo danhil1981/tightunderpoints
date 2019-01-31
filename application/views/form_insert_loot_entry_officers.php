d581d75cb823a6a0e199959710690cb57ccb69af
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-31 20:26:45 +0100

    changed form, now passing list of all character names to choose from

--- application/views/form_insert_loot_entry_officers.php
+++ application/views/form_insert_loot_entry_officers.php
@@ -5,14 +5,16 @@
                     <div id="messages" class="text-center"></div>
                     <br/><br/>
         
-                    <?php echo form_open_multipart('');?>
+                    <?php echo form_open_multipart('officers/insert_drop_loot');?>
 
                     <div class="form-group">
                         <div class="text-white">
                             Character<br/><br/>
                         </div>
-                        <?php echo form_dropdown('id_character', $name_character, $id_character,'disabled="true" class="form-control"');?>
-                        <br/>
+                        <div class="form-inline-block">
+                            <?php echo form_dropdown('id_character', $character_names, $id_character,'class="form-control float-left"');?>
+                        </div>
+                        <br/><br/>
                     </div>
 
                     <div class="form-group">
@@ -43,7 +45,7 @@
                         </div>
                         <div class="form-inline d-block">
                         <?php echo form_dropdown('id_item', '' , '', 'id="item_dropdown" class="form-control float-left"');?>
-                        <button type="button" class='btn btn-primary float-right' id="new_drop" data-toggle="modal" data-target="#modal_drop">New</button>
+                        <button type="button" class='btn btn-primary float-right' id="new_item" data-toggle="modal" data-target="#modal_item">New</button>
                         </div>
                         <br/><br/>
                     </div>
@@ -56,7 +58,7 @@
 
             <div class="modal fade" id="modal_raid" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                    <div class="modal-content">
+                    <div class="modal-content bg-dark text-white">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">New Raid</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -64,16 +66,15 @@
                             </button>
                         </div>
                         <div class="modal-body">
-                            <h1 class="text-center text-white">New Raid</h1>
-            
                             <form id="form_raid">
                                 Description:
                                 <input type="text" id="raid_description" class="form-control"/>
-                                <br/>Date:
+                                <br/>
+                                Date:
                                 <?php echo form_date('date',gmdate('Y-m-d'), 'id="raid_date" required class="form-control"')?>
                                 <br/>
-                                <button id="insert_raid" class="btn btn-primary">Submit</button>
-                                <button class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button id="insert_raid" class="btn btn-success">Submit</button>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                             </form>
                         </div>
                     </div>
@@ -82,7 +83,7 @@
 
             <div class="modal fade" id="modal_event" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                    <div class="modal-content">
+                    <div class="modal-content bg-dark text-white">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">New Event</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -90,55 +91,55 @@
                             </button>
                         </div>
                         <div class="modal-body">
-                            <h1 class="text-center text-white">New Event</h1>
-            
                             <form id="form_event">
                                 Raid:
-                                <select class="form-control" disabled="true" id="selected_raid">
-                                </select>
+                                <select class="form-control" disabled="true" id="selected_raid"></select>
+                                <br/>
                                 Boss:
                                 <?php 
                                     $options = $boss_names;
                                     echo form_dropdown('id_boss', $options, '', 'required id="event_boss_id" class="form-control"');
                                 ?>
+                                <br/>
                                 Time:
                                 <?php $timezone  = +1; echo form_time('time',gmdate("H:i", time()+ 3600*($timezone+date("I"))), 'required id="event_time" class="form-control"')?>
-
+                                <br/>
                                 Date:
                                 <?php echo form_date('date',gmdate('Y-m-d'), 'required id="event_date" class="form-control"')?>
-
-                                <button id="insert_event" class="btn btn-primary">Submit</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <br/>
+                                <button id="insert_event" class="btn btn-success">Submit</button>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="modal fade" id="modal_drop" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="modal_item" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                    <div class="modal-content">
+                    <div class="modal-content bg-dark text-white">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">New Event</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">New Item</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <h1 class="text-center text-white">New Drop</h1>
-            
-                            <form id="form_drop">
+                            <form id="form_item">
                                 Id
-                                <input type="number" class="form-control" id="item_id" min="1" max="32768" required/>
-                                Name
-                                <input type="text" class="form-control" id="item_name" required/>
-                                Drops From
-                                <?php $options = $boss_names; echo form_dropdown('id_boss', $options, '', 'required class="form-control"');?>
-                                Points:
-                                <input type="number" class="form-control" name="value" min="0" max="15" required/>
+                                <input type="number" class="form-control" id="id_item" min="1" max="32768" required/>
                                 <br/>
-                                <button id="insert_item" class="btn btn-primary">Submit</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                Name
+                                <input type="text" class="form-control" id="name_item" required/>
+                                <br/>
+                                Drops From
+                                <?php $options = $boss_names; echo form_dropdown('id_boss', $options, '', 'required disabled id="boss_dropdown" class="form-control"');?>
+                                <br/>
+                                Points
+                                <input type="number" class="form-control" id="value_item" min="0" max="15" required/>
+                                <br/>
+                                <button id="insert_item" class="btn btn-success">Submit</button>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                             </form>
                         </div>
                     </div>
