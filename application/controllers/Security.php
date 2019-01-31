35f2e9e4aa96e2e92a841345739531af3c1d42af
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-01-31 20:49:57 +0100

    removed obsolete methods

--- application/controllers/Security.php
+++ application/controllers/Security.php
@@ -19,34 +19,6 @@
             
         }
 
-        public function admin_panel($data) {
-            $data['users_list'] = $this->model_users->get_all();
-            $data['players_list'] = $this->model_players->get_all();
-            $data['characters_list'] = $this->model_characters->get_all();
-            $data['bosses_list'] = $this->model_bosses->get_all();
-            $data['raids_list'] = $this->model_raids->get_all();
-            $data['items_list'] = $this->model_items->get_all();
-            $data['events_list'] = $this->model_events->get_all();
-            $data['drops_list'] = $this->model_drops->get_all();
-            $data['attendance_list'] = $this->model_attendance->get_all();
-            $data['loot_list'] = $this->model_loot->get_all();
-            $data['view_name'] = 'admin_panel';
-            $this->load->view('template', $data);
-
-        }
-
-        public function officer_panel($data) {
-            $data['list_names'] = $this->model_characters->get_list_names();
-            $data['list_types'] = $this->model_characters->get_list_types();
-            $data['list_total_earned'] = $this->model_characters->get_list_total_earned();
-            $data['list_last50_earned'] = $this->model_characters->get_list_last50_earned();
-            $data['list_total_spent'] = $this->model_characters->get_list_total_spent();
-            $data['list_last50_spent'] = $this->model_characters->get_list_last50_spent();
-            $data['timers'] = $this->model_officers->get_timers();
-            $data['view_name'] = 'officer_panel';
-            $this->load->view('template',$data);
-        }
-
         public function process_login() {
 
             $result = $this->model_users->validate();            
