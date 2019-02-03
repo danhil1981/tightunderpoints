7847f90dd6ef4f1f0b9f25dab64002df1224ccb1
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-03 18:15:13 +0100

    fixed cdn link for datatables style sheets

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -1,5 +1,5 @@
             <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/custom.css">
-            <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/tables.bootstrap4.min.css">
+            <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
             <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
             <script src="<?php echo base_url()?>assets/js/admin_panel.js"></script>
             <h1 class='text-center text-white'>Admin Panel</h1>
