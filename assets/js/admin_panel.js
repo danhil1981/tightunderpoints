77226d76095447cd26160023bc535515fb71dbc3
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-03 17:53:06 +0100

    switch to jquery from vanilla

--- assets/js/admin_panel.js
+++ assets/js/admin_panel.js
@@ -1,14 +1,15 @@
-tables = ['users', 'players', 'characters', 'raids', 'bosses', 'items', 'events', 'drops', 'attendance','loot'];
-
 $(document).ready(function () {
     $("#tables table").DataTable({"order": [[1, "asc"]]});
+
+    $('#menu_buttons').on('click', "button", function () {
+        $('#menu_buttons button').removeClass('btn-primary').addClass('btn-light');
+        $(this).removeClass('btn-light').addClass('btn-primary');
+        $('#tables').children().removeClass('d-block').addClass('d-none');
+        $('#'+this.id.slice(7)).removeClass('d-none').addClass('d-block');
+    });
 });
 
 function show(table) {
-    for (i=0; i<tables.length;i++) {
-        document.getElementById("button_" + tables[i]).className = "btn btn-light btn-sm";
-        document.getElementById(tables[i]).style.display = "none";
-    }
-    document.getElementById("button_"+table).className = "btn btn-primary btn-sm";
-    document.getElementById(table).style.display = "block";
+    $('#button_'+table).removeClass('btn-light').addClass('btn-primary');
+    $('#'+table).removeClass('d-none').addClass('d-block');
 }
