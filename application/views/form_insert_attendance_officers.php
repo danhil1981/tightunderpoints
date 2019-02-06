b2976b044b01c26b20f7c76bc5ccd2204875aa03
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-06 09:43:49 +0100

    corrected button size, added styled file input

--- application/views/form_insert_attendance_officers.php
+++ application/views/form_insert_attendance_officers.php
@@ -13,13 +13,16 @@
                     </div>
                     <?php $options = $character_names;?>
                     <div class="form-group">
-                        <div class="text-white">
+                        <div id="label_characters" class="text-white">
                             Character<br/><br/>
                         </div>
                         <div class="form-inline d-block">
-                            <?php echo form_dropdown('id_character', $options, '', 'required id="character_dropdown" class="form-control float-left"');?>
-                            <button id="add_character" class="btn btn-sm btn-primary form-control float-left ml-2">Add</button>
-                            <?php echo form_upload('list_characters','','class="float-right"');?>
+                            <?php echo form_dropdown('id_character', $options, '', 'required id="character_dropdown" class="manual_input form-control float-left"');?>
+                            <button id="add_character" class="manual_input btn btn-sm btn-primary form-control float-left ml-2">Add</button>
+                            <label class="btn btn-sm btn-primary float-right" for="upload_input">
+                                <?php echo form_upload('list_characters','','id="upload_input" class="d-none"');?>Upload Log
+                            </label>
+                            <div class='text-white float-right mr-2' id="upload-file-info"></div>
                         </div>
                         <br/><br/><br/>
                     </div>
@@ -37,8 +40,8 @@
                     </div>
                     <?php echo form_hidden('id_event',$id_event);?>
                     <input type="hidden" name="list_characters" id="list_characters" value="" />
-                    <?php echo form_submit('submit', 'Submit', 'class="btn btn-success btn-small"');?>
-                    <?php echo anchor('officers', 'Cancel', 'class="btn btn-danger btn-small"');?>
+                    <?php echo form_submit('submit', 'Submit', 'class="btn btn-success btn-sm"');?>
+                    <?php echo anchor('officers', 'Cancel', 'class="btn btn-danger btn-sm"');?>
                     <?php echo form_close();?>
                 </div>
             </div>
