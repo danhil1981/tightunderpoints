6ffc6b073829a149e8d754b3935d8663afa5f8ba
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-07 14:25:25 +0100

    Added some classes, inputs and buttons

--- application/views/form_confirm_attendance.php
+++ application/views/form_confirm_attendance.php
@@ -3,7 +3,7 @@
             <br/><br/>
             <div class="row">
                 <div class="col-8 offset-2" id="tables">
-                    <?php echo form_open_multipart('officers/confirm_attendance');?>
+                    <?php echo form_open_multipart('officers/insert_attendance');?>
                         <table class='table table-dark table-striped table-bordered table-hover table-sm text-center' id="table_users">
                             <thead>
                                 <tr>
@@ -16,19 +16,23 @@
                                 <?php
                                     foreach ($list_characters as $i => $value) {
                                         echo "<tr id='".$value."'>";
-                                        echo "<td>".$character_names[$value]."</td>";
+                                        echo "<td><input type='text' hidden name='character_".$value."'/>".$character_names[$value]."</td>";
                                         if ($list_types[$value] != 1) {
                                             echo "<td><input id='botted_".$value."' type='checkbox' name='botted' class='checks form-control' checked='true' /></td>";
                                         }
                                         else {
                                             echo "<td><input id='botted_".$value."' type='checkbox' name='botted' class='checks form-control' /></td>";
                                         }
-                                        echo "<td>".form_dropdown('id_substituting_'.$value, $list_mains, '', 'id="dropdown_'.$value.'"class="form-control d-none"')."</td>";
+                                        echo "<td>".form_dropdown('id_substituting_'.$value, array(0 => "-- Select a character --")+$list_mains, '', 'id="dropdown_'.$value.'"class="dropdowns form-control d-none"')."</td>";
                                         echo "</tr>";
                                     }
                                 ?>
                             </tbody>
                         </table>
+                        <?php echo form_hidden('id_event',$id_event);?>
+                        <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-sm"');?>
+                        <?php echo anchor('officers', 'Cancel', 'class="btn btn-danger btn-sm"');?>
+                        <?php echo form_close();?>
                     </form>
                 </div>
             </div>
