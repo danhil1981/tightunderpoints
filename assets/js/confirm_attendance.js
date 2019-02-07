bea3f79d600187f0dbfd2ecb4ae0a3263ff7d742
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-07 14:24:02 +0100

    added new functions for when the checkboxes and dropdowns are changed

--- assets/js/confirm_attendance.js
+++ assets/js/confirm_attendance.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
+    
     $(".checks:checkbox:checked").each(function() {
-        id = this.id.slice(7);
+        var id = this.id.slice(7);
         $("#dropdown_"+id).removeClass("d-none").addClass("d-block");
     });
     
+    $(".checks:checkbox").change(function() {
+        var id = this.id.slice(7);
+        var ischecked = $(this).is(':checked');
+        if (ischecked) {
+            $("#dropdown_" + id).removeClass("d-none").addClass("d-block");
+        }
+        else {
+            $("#dropdown_" + id).removeClass("d-block").addClass("d-none");
+            $("#dropdown_" + id).prop("selectedIndex", 0)
+        }
+    });
+
+    $(".dropdowns").change(function() {
+        var id_dropdown = this.id.slice(9);
+        var id_main = $("#"+this.id+" option:selected").val();
+        $("#"+this.id).removeClass("dropdowns");
+        $('#' + this.id + ' option[value!='+id_main+']').remove();
+        $(".dropdowns").each(function() {
+            $("#"+this.id+" option[value='"+id_main+"']").remove();
+        });
+    });
 
 });
