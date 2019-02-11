9ee50b70caeed5d7125d45df7514ed5e1ca5845a
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-11 09:28:42 +0100

    Added additional dropdown for "played by"

--- application/views/form_insert_attendance_entry.php
+++ application/views/form_insert_attendance_entry.php
@@ -19,6 +19,14 @@
                         <?php echo form_dropdown('id_character', $options, '', 'required class="form-control"');?>
                         <br/><br/>
                     </div>
+                    <?php $options = $mains_names;?>
+                    <div class="form-group">
+                        <div class="text-white">
+                            Played By<br/><br/>
+                        </div>
+                        <?php echo form_dropdown('id_points', $options, '', 'required class="form-control"');?>
+                        <br/><br/>
+                    </div>
                     <?php echo form_submit('submit', 'Submit', 'class="btn btn-primary btn-sm"');?>
                     <?php echo anchor('admins', 'Cancel', 'class="btn btn-danger btn-sm"');?>
                     <?php echo form_close();?>
