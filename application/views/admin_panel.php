5c0c37758ab078638fc17d04e6b2e1f34ac72dde
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-12 14:07:35 +0100

    Added link to member panel

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -20,6 +20,7 @@
                     <button id='button_loot' class="btn btn-light btn-sm">Loot</button>
                     <a href='<?php echo site_url()?>' class='btn btn-danger btn-sm'>Logout</a>
                     <?php echo anchor('officers', 'Officer Panel', 'class="btn btn-success btn-sm"');?>
+                    <?php echo anchor('members', 'Member Panel', 'class="btn btn-success btn-sm"');?>
                     <br/><br/>
                 </div>
             </div>
