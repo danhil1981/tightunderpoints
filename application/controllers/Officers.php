b9f561918fcdf6c8aca7fd3efb6171cf39538bc6
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-12 14:06:19 +0100

    Added security check to index

--- application/controllers/Officers.php
+++ application/controllers/Officers.php
@@ -5,20 +5,22 @@
     Class Officers extends Security {
 
         public function index() {
-            $data['list_names'] = $this->model_characters->get_list_names();
-            $data['list_types'] = $this->model_characters->get_list_types();
-            $data['list_total_earned'] = $this->model_characters->get_list_total_earned();
-            $data['list_last50_earned'] = $this->model_characters->get_list_last50_earned();
-            $data['list_total_spent'] = $this->model_characters->get_list_total_spent();
-            $data['list_last50_spent'] = $this->model_characters->get_list_last50_spent();
-            $data['timers'] = $this->model_officers->get_timers();
-            $data['events'] = $this->model_events->get_list();
-            $data['attendance_list'] = $this->model_attendance->get_all();
-            $data['played_list'] = $this->model_attendance->get_played();
-            $data['players_list'] = $this->model_players->get_all();
-            $data['characters_list'] = $this->model_characters->get_all();
-            $data['view_name'] = 'officer_panel';
-            $this->load->view('template', $data);
+            if ($this->check_permission(2)) {
+                $data['list_names'] = $this->model_characters->get_list_names();
+                $data['list_types'] = $this->model_characters->get_list_types();
+                $data['list_total_earned'] = $this->model_characters->get_list_total_earned();
+                $data['list_last50_earned'] = $this->model_characters->get_list_last50_earned();
+                $data['list_total_spent'] = $this->model_characters->get_list_total_spent();
+                $data['list_last50_spent'] = $this->model_characters->get_list_last50_spent();
+                $data['timers'] = $this->model_officers->get_timers();
+                $data['events'] = $this->model_events->get_list();
+                $data['attendance_list'] = $this->model_attendance->get_all();
+                $data['played_list'] = $this->model_attendance->get_played();
+                $data['players_list'] = $this->model_players->get_all();
+                $data['characters_list'] = $this->model_characters->get_all();
+                $data['view_name'] = 'officer_panel';
+                $this->load->view('template', $data);
+            }
         }
 
         public function loot($id) {
