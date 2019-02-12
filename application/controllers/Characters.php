9ab516f5ff0c1599ec26e5e639b6bcf04d9d79d5
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-12 12:33:12 +0100

    Added source as parameter to functions to redirect to correct controller

--- application/controllers/Characters.php
+++ application/controllers/Characters.php
@@ -4,16 +4,18 @@
 
     Class Characters extends Security {
 
-        public function show_insert() {
+        public function show_insert($source = "admins") {
             if ($this->check_login()) {
                 $data['view_name'] = 'form_insert_character';
                 $data['player_names'] = $this->model_players->get_list();
+                $data['source'] = $source;
                 $this->load->view('template', $data);
             }
         }
 
         public function insert() {
             if ($this->check_login()) {
+                $source = $this->input->post("source");
                 $result_insert = $this->model_characters->insert();
                 if ($result_insert == 0) {
                     $this->session->set_flashdata("msg","<div class='badge badge-danger'>Error on insertion</div><br/>");
@@ -22,11 +24,14 @@
                     $this->session->set_flashdata("msg","<div class='badge badge-success'>Character successfully inserted</div><br/>");
                 }
                 $this->session->set_flashdata("table", "characters");
+                if ($source == "officers") {
+                    redirect('officers');
+                }
                 redirect('admins');
             }
         }
 
-        public function delete($id) {
+        public function delete($id, $source = "admins") {
             if ($this->check_login()) {
                 $result = $this->model_characters->delete($id);
                 if ($result == 0) {
@@ -36,21 +41,26 @@
                     $this->session->set_flashdata("msg","<div class='badge badge-success'>Character successfully deleted</div><br/>");
                 }
                 $this->session->set_flashdata("table", "characters");
+                if ($source == "officers") {
+                    redirect('officers');
+                }
                 redirect('admins');
             }
         }
 
-        public function show_modify($id) {
+        public function show_modify($id, $source = "admins") {
             if ($this->check_login()) {
                 $data['view_name'] = 'form_modify_character';
                 $data['character'] = $this->model_characters->get($id);
                 $data['player_names'] = $this->model_players->get_list();
+                $data['source'] = $source;
                 $this->load->view('template', $data);
             }
         }
 
         public function modify() {
             if ($this->check_login()) {
+                $source = $this->input->post("source");
                 $result = $this->model_characters->modify();
                 if ($result == 0) {
                     $this->session->set_flashdata("msg","<div class='badge badge-danger'>Error on modification</div><br/>");
@@ -59,6 +69,9 @@
                     $this->session->set_flashdata("msg","<div class='badge badge-success'>Character successfully modified</div><br/>");
                 }
                 $this->session->set_flashdata("table", "characters");
+                if ($source == "officers") {
+                    redirect('officers');
+                }
                 redirect('admins');
             }
         }
