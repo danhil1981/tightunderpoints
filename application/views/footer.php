d804af6651c488b11789d5e3bedadd57b59e764d
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-13 12:06:33 +0100

    changed link to open new tab

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
             <br/>
             <br/>
             <div class="text-center">
-                <a href="https://github.com/danhil1981/tightunderpoints" class="badge badge-primary">&copy; Daniel Hilbrand 2018-2019</a>
+                <a href="https://github.com/danhil1981/tightunderpoints"  target="_blank" class="badge badge-primary">&copy; Daniel Hilbrand 2018-2019</a>
             </div>
         </div>
     </body>
