3e1a06418d489e759c9ab44dc4bc7420389fd769
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-14 11:06:17 +0100

    added length restriction for input fields, added title for regex

--- application/views/form_insert_user.php
+++ application/views/form_insert_user.php
@@ -7,7 +7,7 @@
                         <div class="text-white">
                             Name<br/><br/>
                         </div>
-                        <?php echo form_input('name','','required pattern="^[A-Za-z]+$" class="form-control"')?>
+                        <?php echo form_input('name','','required pattern="^[A-Za-z]+$" title="1 word consisting of uppercase/lowercase letters" maxlength="32" class="form-control"')?>
                     </div>
                     <div class="form-group">
                         <div class="text-white">
