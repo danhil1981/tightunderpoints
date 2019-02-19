11997f54a82adf8cd02dce22a7ea33667ee329ce
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-19 14:08:22 +0100

    Added a new method that returns detailed information for a character

--- application/models/Model_members.php
+++ application/models/Model_members.php
@@ -49,6 +49,81 @@
             return $names;
         }
 
+        public function show_character($id_character) {
+            $query = $this->db->query("SELECT
+                characters.name AS 'name_character',
+                characters.level AS 'level_character',
+                characters.class AS 'class_character',
+                characters.type AS 'type_character',
+                players.name AS 'name_player',
+                IFNULL(SUM(items.value),0) AS last50_spent
+                FROM characters
+                INNER JOIN loot ON characters.id = loot.id_character
+                INNER JOIN drops ON loot.id_drop = drops.id
+                INNER JOIN items ON drops.id_item = items.id
+                INNER JOIN events ON drops.id_event = events.id
+                INNER JOIN players ON characters.id_player = players.id
+                WHERE events.timestamp >= DATE_SUB(NOW(), INTERVAL 50 DAY) AND characters.id = $id_character;  
+            ;");
+            $character = $query->result_array()[0];
+            $query = $this->db->query("SELECT
+                IFNULL(SUM(items.value),0) AS total_spent
+                FROM characters
+                INNER JOIN loot ON characters.id = loot.id_character
+                INNER JOIN drops ON loot.id_drop = drops.id
+                INNER JOIN items ON drops.id_item = items.id
+                INNER JOIN events ON drops.id_event = events.id
+                WHERE characters.id = $id_character;  
+            ;");
+            $character += $query->result_array()[0];
+            $query = $this->db->query("SELECT
+                IFNULL(SUM(bosses.value),0) AS last50_earned
+                FROM characters
+                INNER JOIN attendance ON characters.id = attendance.id_points
+                INNER JOIN events ON attendance.id_event = events.id
+                INNER JOIN bosses ON events.id_boss = bosses.id
+                WHERE events.timestamp >= DATE_SUB(NOW(), INTERVAL 50 DAY) AND id_character = $id_character
+            ;");
+            $character += $query->result_array()[0];
+            $query = $this->db->query("SELECT
+                IFNULL(SUM(bosses.value),0) AS total_earned
+                FROM characters
+                INNER JOIN attendance ON characters.id = attendance.id_points
+                INNER JOIN events ON attendance.id_event = events.id
+                INNER JOIN bosses ON events.id_boss = bosses.id
+                WHERE id_character = $id_character
+            ;");
+            $character += $query->result_array()[0];
+            $query = $this->db->query("SELECT
+                events.timestamp AS timestamp_last_event,
+                bosses.name AS boss_last_event
+                FROM characters
+                INNER JOIN attendance ON characters.id = attendance.id_points
+                INNER JOIN events ON attendance.id_event = events.id
+                INNER JOIN bosses ON events.id_boss = bosses.id
+                WHERE id_character = $id_character
+                ORDER BY events.timestamp DESC LIMIT 1
+            ;");
+            if (isset($query->result_array()[0])) {
+                $character += $query->result_array()[0];
+            }
+            $query = $this->db->query("SELECT
+                events.timestamp AS timestamp_last_loot,
+                items.name AS item_last_loot
+                FROM characters
+                INNER JOIN loot ON characters.id = loot.id_character
+                INNER JOIN drops ON loot.id_drop = drops.id
+                INNER JOIN events ON drops.id_event = events.id
+                INNER JOIN items ON drops.id_item = items.id
+                WHERE id_character = $id_character
+                ORDER BY events.timestamp DESC LIMIT 1
+            ;");
+            if (isset($query->result_array()[0])) {
+                $character += $query->result_array()[0];
+            }
+            return $character;
+        }
+
     }
 
 ?>
