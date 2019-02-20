933410240224c9fdc5b4fa8d152427ff5e596a46
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-20 13:15:10 +0100

    Added boss modal

--- application/views/member_panel.php
+++ application/views/member_panel.php
@@ -290,6 +290,20 @@
                     </div>
                 </div>
             </div>
+            <div class="modal fade" id="modal_boss" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content bg-dark text-white">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="title_boss"></h5>
+                            <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center" id="content_boss">
+                        </div>
+                    </div>
+                </div>
+            </div>
             <?php 
                 if(isset($this->session->table)) {
                     echo "<script>show('".$this->session->table."')</script>";
