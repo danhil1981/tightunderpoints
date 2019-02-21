485c72414ac2bbe178e52600166b5fdd821757b3
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-21 12:36:24 +0100

    now loading drop list to get item id in loot table

--- application/controllers/members.php
+++ application/controllers/members.php
@@ -18,6 +18,7 @@
                 $data['raids_list'] = $this->model_raids->get_all();
                 $data['items_list'] = $this->model_items->get_all();
                 $data['events_list'] = $this->model_events->get_all();
+                $data['drop_list'] = $this->model_drops->get_drops_items();
                 $data['view_name'] = 'member_panel';
                 $this->load->view('template', $data);
             }
@@ -50,6 +51,14 @@
             if ($this->check_permission(3)) {
                 $id_boss = $this->input->post("id_boss");
                 print_r(json_encode($this->model_members->list_items($id_boss)));
+                die;
+            }
+        }
+
+        public function show_item() {
+            if ($this->check_permission(3)) {
+                $id_item = $this->input->post("id_item");
+                print_r(json_encode($this->model_members->show_item($id_item)));
                 die;
             }
         }
