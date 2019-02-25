5fd535d3caa9473f436ced7c3f43e414ee083bc9
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-02-25 13:14:38 +0100

    Limited default rows to 10 so the "New" buttons are easily accessible

--- assets/js/admin_panel.js
+++ assets/js/admin_panel.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
     $("#table_users, #table_raids, #table_drops, #table_loot").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [3, 4], "orderable": false }]
     });
 
     $("#table_players").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [2, 3], "orderable": false }]
     });
 
     $("#table_characters").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [6, 7], "orderable": false }]
     });
 
     $("#table_bosses").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [5, 6], "orderable": false }]
     });
 
     $("#table_items").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [4, 5, 6], "orderable": false }]
     });
 
     $("#table_events, #table_attendance").DataTable({
         "order": [[0, "asc"]],
-        "lengthMenu": [50, 100, 500],
+        "lengthMenu": [10, 50, 100, 500],
         "columnDefs": [{ "targets": [4, 5], "orderable": false }]
     });
 
