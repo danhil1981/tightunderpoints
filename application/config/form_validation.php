e6292e2a476466fea2e772dd9e07e88f54f09132
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-04-12 10:39:42 +0200

    Added validation rules for all forms

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -13,6 +13,46 @@
                 'rules' => 'required|max_length[32]',
             ],
         ],
+        'players' => [
+            [
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|max_length[32]',
+            ],
+        ],
+        'characters' => [
+            [
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|max_length[32]',
+            ],
+        ],
+        'bosses' => [
+            [
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|max_length[64]',
+            ],
+        ],
+        'items' => [
+            [
+                'field' => 'id',
+                'label' => 'ID',
+                'rules' => 'required|integer|greater_than[0]|less_than[32769]',
+            ],
+            [
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|max_length[64]',
+            ],
+        ],
+        'raids' => [
+            [
+                'field' => 'description',
+                'label' => 'Description',
+                'rules' => 'required|max_length[512]',
+            ],
+        ],
     ];
 
     $config['error_prefix'] = '<div class="badge badge-danger">';
