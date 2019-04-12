d485e8a5bc9ad215f5c27c9bcd7807cfda14ec6c
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-04-12 10:52:06 +0200

    Added server side form validation

--- application/controllers/characters.php
+++ application/controllers/characters.php
@@ -17,23 +17,36 @@
         public function insert()
         {
             if ($this->check_permission(2)) {
-                $source = $this->input->post('source');
-                $name = $this->input->post('name');
-                $level = $this->input->post('level');
-                $class = $this->input->post('class');
-                $type = $this->input->post('type');
-                $id_player = $this->input->post('id_player');
-                $result_insert = $this->model_characters->insert($name, $level, $class, $type, $id_player);
-                if ($result_insert == 0) {
-                    $this->session->set_flashdata('msg', "<div class='badge badge-danger'>Database Error</div><br/>");
+                if ($this->form_validation->run('characters') == false) {
+                    $data['view_name'] = 'form_insert_character';
+                    $data['msg'] = validation_errors();
+                    $data['source'] = $this->input->post('source');
+                    $data['player_names'] = $this->model_players->get_list();
+                    $data['character']['name'] = $this->input->post('name');
+                    $data['character']['level'] = $this->input->post('level');
+                    $data['character']['class'] = $this->input->post('class');
+                    $data['character']['type'] = $this->input->post('type');
+                    $data['character']['id_player'] = $this->input->post('id_player');
+                    $this->load->view('template', $data);
                 } else {
-                    $this->session->set_flashdata('msg', "<div class='badge badge-success'>Character successfully created</div><br/>");
+                    $source = $this->input->post('source');
+                    $name = $this->input->post('name');
+                    $level = $this->input->post('level');
+                    $class = $this->input->post('class');
+                    $type = $this->input->post('type');
+                    $id_player = $this->input->post('id_player');
+                    $result_insert = $this->model_characters->insert($name, $level, $class, $type, $id_player);
+                    if ($result_insert == 0) {
+                        $this->session->set_flashdata('msg', "<div class='badge badge-danger'>Database Error</div><br/>");
+                    } else {
+                        $this->session->set_flashdata('msg', "<div class='badge badge-success'>Character successfully created</div><br/>");
+                    }
+                    $this->session->set_flashdata('table', 'characters');
+                    if ($source == 'officers') {
+                        redirect('officers');
+                    }
+                    redirect('admins');
                 }
-                $this->session->set_flashdata('table', 'characters');
-                if ($source == 'officers') {
-                    redirect('officers');
-                }
-                redirect('admins');
             }
         }
 
@@ -68,23 +81,38 @@
         public function modify()
         {
             if ($this->check_permission(2)) {
-                $source = $this->input->post('source');
-                $id = $this->input->post('id');
-                $name = $this->input->post('name');
-                $level = $this->input->post('level');
-                $type = $this->input->post('type');
-                $id_player = $this->input->post('id_player');
-                $result = $this->model_characters->modify($id, $name, $level, $class, $type, $id_player);
-                if ($result == 0) {
-                    $this->session->set_flashdata('msg', "<div class='badge badge-danger'>Database Error</div><br/>");
+                if ($this->form_validation->run('characters') == false) {
+                    $data['view_name'] = 'form_modify_character';
+                    $data['msg'] = validation_errors();
+                    $data['source'] = $this->input->post('source');
+                    $data['player_names'] = $this->model_players->get_list();
+                    $data['character']['id'] = $this->input->post('id');
+                    $data['character']['name'] = $this->input->post('name');
+                    $data['character']['level'] = $this->input->post('level');
+                    $data['character']['class'] = $this->input->post('class');
+                    $data['character']['type'] = $this->input->post('type');
+                    $data['character']['id_player'] = $this->input->post('id_player');
+                    $this->load->view('template', $data);
                 } else {
-                    $this->session->set_flashdata('msg', "<div class='badge badge-success'>Character successfully modified</div><br/>");
+                    $source = $this->input->post('source');
+                    $id = $this->input->post('id');
+                    $name = $this->input->post('name');
+                    $level = $this->input->post('level');
+                    $class = $this->input->post('class');
+                    $type = $this->input->post('type');
+                    $id_player = $this->input->post('id_player');
+                    $result = $this->model_characters->modify($id, $name, $level, $class, $type, $id_player);
+                    if ($result == 0) {
+                        $this->session->set_flashdata('msg', "<div class='badge badge-danger'>Database Error</div><br/>");
+                    } else {
+                        $this->session->set_flashdata('msg', "<div class='badge badge-success'>Character successfully modified</div><br/>");
+                    }
+                    $this->session->set_flashdata('table', 'characters');
+                    if ($source == 'officers') {
+                        redirect('officers');
+                    }
+                    redirect('admins');
                 }
-                $this->session->set_flashdata('table', 'characters');
-                if ($source == 'officers') {
-                    redirect('officers');
-                }
-                redirect('admins');
             }
         }
     }
--- application/views/form_modify_character.php
+++ application/views/form_modify_character.php
@@ -1,46 +1,53 @@
             <div class="row">
                 <div class="col-6 offset-3">
                     <h1 class="text-center text-white">Modify Character</h1>
-                    <br/><br/>
+                    <br /><br />
+                    <div class="text-center">
+                        <?php
+                            if (isset($msg)) {
+                                echo $msg;
+                            }
+                        ?>
+                    </div>
                     <?php echo form_open('characters/modify');?>
                     <div class="form-group">
                         <div class="text-white">
-                            Name<br/><br/>
+                            Name<br /><br />
                         </div>
                         <?php echo form_input('name', $character['name'], "required maxlength='32' pattern='^[A-Za-z]+$' title='1 word consisting of uppercase/lowercase letters' class='form-control'")?>
-                        <br/>
+                        <br />
                     </div>
                     <?php $options = array_combine(range(1, 65), range(1, 65));?>
                     <div class="form-group">
                         <div class="text-white">
-                            Level<br/><br/>
+                            Level<br /><br />
                         </div>
                         <?php echo form_dropdown('level', $options, $character['level'], "required class='form-control'");?>
-                        <br/>
+                        <br />
                     </div>
                     <?php $options = ['BRD' => 'BRD', 'BST' => 'BST', 'CLR' => 'CLR', 'DRU' => 'DRU', 'ENC' => 'ENC', 'MAG' => 'MAG', 'MNK' => 'MNK', 'NEC' => 'NEC', 'PAL' => 'PAL', 'RNG' => 'RNG', 'ROG' => 'ROG', 'SHD' => 'SHD', 'SHM' => 'SHM', 'WAR' => 'WAR', 'WIZ' => 'WIZ'];?>
                     <div class="form-group">
                         <div class="text-white">
-                            Class<br/><br/>
+                            Class<br /><br />
                         </div>
                         <?php echo form_dropdown('class', $options, $character['class'], "required class='form-control'");?>
-                        <br/>
+                        <br />
                     </div>
                     <?php $options = ['1' => 'Main', '2' => 'Alt', '3' => 'Bot'];?>
                     <div class="form-group">
                         <div class="text-white">
-                            Type<br/><br/>
+                            Type<br /><br />
                         </div>
                         <?php echo form_dropdown('type', $options, $character['type'], "required class='form-control'");?>
-                        <br/>
+                        <br />
                     </div>
                     <?php $options = $player_names;?>
                     <div class="form-group">
                         <div class="text-white">
-                            Player<br/><br/>
+                            Player<br /><br />
                         </div>
                         <?php echo form_dropdown('id_player', $options, $character['id_player'], "required class='form-control'");?>
-                        <br/><br/>
+                        <br /><br />
                     </div>
                     <?php echo form_submit('submit', 'Modify', "class='btn btn-success btn-sm'");?>
                     <?php echo form_hidden('source', $source);?>
