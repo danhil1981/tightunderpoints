dfa8655f5c0dc260820d7b72b1b108977d0ed9b8
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-05-03 10:17:10 +0200

    extracted modal to partial

---
+++ application/views/modals/delete_confirmation.php
@@ -0,0 +1,22 @@
+            <div class="modal fade" id="modal_delete_confirmation" tabindex="-1" role="dialog"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content bg-dark text-white">
+                        <div class="modal-header">
+                            <h5 class="modal-title w-100 text-center" id="exampleModalLabel">EMPTY</h5>
+                            <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                        </div>
+                        <div class="modal-footer justify-content-center">
+                            <form id="form_delete_confirmation" action="EMPTY" method="GET">
+                                <button type="submit" class="btn btn-danger d-inline-block">Delete</button>
+                                <button type="button" class="btn btn-warning d-inline-block"
+                                    data-dismiss="modal">Cancel</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
