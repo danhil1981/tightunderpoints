7454bc5e7c1a24f66dcdae955a00a55b22c91759
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-05-10 09:29:38 +0200

    created new partial for ui messages

---
+++ application/views/messages/message.php
@@ -0,0 +1,7 @@
+            <div class="text-center my-5">
+                <?php
+                    if (isset($this->session->msg)) {
+                        echo $this->session->msg;
+                    }
+                ?>
+            </div>
