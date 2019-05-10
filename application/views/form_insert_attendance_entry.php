972e057d92687ddd4c4117681664268b8fd2acbb
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-05-10 11:03:21 +0200

    removed line breaks, design changes

--- application/views/form_insert_attendance_entry.php
+++ application/views/form_insert_attendance_entry.php
@@ -1,34 +1,32 @@
-            <div class="row">
+            <div class="row my-5">
                 <div class="col-6 offset-3">
                     <h1 class="text-center text-white">New Attendance Entry</h1>
-                    <br/><br/>
                     <?php echo form_open('attendance/insert');?>
                     <?php $options = $event_names;?>
                     <div class="form-group">
-                        <div class="text-white">
-                            Event<br/><br/>
+                        <div class="text-white mt-5 mb-2">
+                            Event
                         </div>
                         <?php echo form_dropdown('id_event', $options, '', "required class='form-control'");?>
-                        <br/><br/>
                     </div>
                     <?php $options = $character_names;?>
                     <div class="form-group">
-                        <div class="text-white">
-                            Character<br/><br/>
+                        <div class="text-white mt-5 mb-2">
+                            Character
                         </div>
                         <?php echo form_dropdown('id_character', $options, '', "required class='form-control'");?>
-                        <br/><br/>
                     </div>
                     <?php $options = $main_names;?>
                     <div class="form-group">
-                        <div class="text-white">
-                            Played By<br/><br/>
+                        <div class="text-white mt-5 mb-2">
+                            Played By
                         </div>
                         <?php echo form_dropdown('id_points', $options, '', "required class='form-control'");?>
-                        <br/><br/>
                     </div>
-                    <?php echo form_submit('submit', 'Create', "class='btn btn-success btn-sm'");?>
-                    <?php echo anchor('admins', 'Cancel', "class='btn btn-danger btn-sm'");?>
+                    <div class="text-center mt-5">
+                        <?php echo form_submit('submit', 'Create', "class='btn btn-success btn-sm'");?>
+                        <?php echo anchor('admins', 'Cancel', "class='btn btn-danger btn-sm'");?>
+                    </div>
                     <?php echo form_close();?>
                 </div>
             </div>
