d43a2e077a1cab9d490b94ee42011cbe48a5a4ac
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-05-10 10:59:33 +0200

    removed line breaks

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,3 @@
-            <br/>
-            <br/>
             <div class="text-center">
                 <a href="https://github.com/danhil1981/tightunderpoints"  target="_blank" class="badge badge-primary">&copy; Daniel Hilbrand 2018-2019</a>
             </div>
