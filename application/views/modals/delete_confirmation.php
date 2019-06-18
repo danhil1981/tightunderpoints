be902f702bc285a8a00d2151982554543e15c1a2
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-06-18 11:01:32 +0200

    change colors

--- application/views/modals/delete_confirmation.php
+++ application/views/modals/delete_confirmation.php
@@ -1,20 +1,17 @@
             <div class="modal fade" id="modal_delete_confirmation" tabindex="-1" role="dialog"
                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                    <div class="modal-content bg-dark text-white">
+                    <div class="modal-content bg-light text-white">
                         <div class="modal-header">
                             <h5 class="modal-title w-100 text-center" id="exampleModalLabel">EMPTY</h5>
-                            <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
                         </div>
                         <div class="modal-body text-center">
                         </div>
                         <div class="modal-footer justify-content-center">
                             <form id="form_delete_confirmation" action="EMPTY" method="GET">
-                                <button type="submit" class="btn btn-danger d-inline-block">Delete</button>
-                                <button type="button" class="btn btn-warning d-inline-block"
-                                    data-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-sm btn-success d-inline-block w-40"><i class="material-icons align-middle">delete</i><span class="align-middle"> Delete</span></button>
+                                    <button type="button" class="btn btn-sm btn-danger d-inline-block w-40"
+                                        data-dismiss="modal"><i class="material-icons align-middle">cancel</i><span class="align-middle"> Cancel</span></button>
                             </form>
                         </div>
                     </div>
