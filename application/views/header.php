7f3a79efa310c0daea456737255ed34c5c7c2d1d
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-06-18 13:24:26 +0200

    change header

--- application/views/header.php
+++ application/views/header.php
@@ -8,14 +8,26 @@
         <meta name="description" content="Tight Underpoints - An application for DKP Management"/>
         <meta name="language" content="EN"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/img/favicon.PNG">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/img/logo.ico">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <style>
+            body {
+                background-image: url(<?php echo base_url()?>assets/img/pattern.png);
+                background-repeat: repeat;
+            }
+            .logo-text {
+                color: #286d67;
+                font-weight: bold;
+                text-shadow: #D74B27 2px 2px 4px;
+                -webkit-text-stroke: 1px black;
+            }
+        </style>
     </head>
-    <body class="bg-dark">
+    <body>
         <div class="container-fluid">
             <div class="row">
                 <div class="mx-auto">
@@ -34,7 +46,7 @@
                                 }
                         }
                     ?>
-                    <img src="<?php echo base_url()?>assets/img/logo.PNG" alt="Responsive Logo">
+                    <img src="<?php echo base_url()?>assets/img/logo.PNG" alt="Responsive Logo" height="150px"><h1 class="logo-text d-inline-block ml-3 pt-5">Tight Underpoints</h1>
                     <?php
                         if (isset($this->session->logged_in)) {
                             echo '</a>';
