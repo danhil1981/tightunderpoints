06ef565cde1f1f12e31c30bdf1afdc00b10656e2
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-06-19 10:42:23 +0200

    change link text

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
             <div class="col-10 offset-1 text-right">
-                <a href="https://github.com/danhil1981/tightunderpoints" target="_blank" class="text-success">Report bugs here  <img height="30px" src="<?php echo base_url()?>assets/img/github.png" alt="Github Logo"></a>
+                <a href="https://github.com/danhil1981/tightunderpoints" target="_blank" class="text-success">&copy; danhil webdesign  <img height="30px" src="<?php echo base_url()?>assets/img/github.png" alt="Github Logo"></a>
             </div>
         </div>
     </body>
