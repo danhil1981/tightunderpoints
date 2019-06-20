7a116f40ca7eebfe4949e069e0feeff70efd7ab5
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-06-20 17:23:03 +0200

    remove a trailing symbol

--- application/views/tables/players.php
+++ application/views/tables/players.php
@@ -9,7 +9,7 @@
                                 <tr>
                                     <th scope="col">Id</th>
                                     <th scope="col">Name</th>
-                                    <th scope="col""></th>
+                                    <th scope="col"></th>
                                 </tr>
                             </thead>
                             <tbody>
