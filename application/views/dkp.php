6efd4d436057ca84adba666ea86ab5454438c3ab
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-07-10 20:25:29 +0200

    create FAQ

---
+++ application/views/dkp.php
@@ -0,0 +1,12 @@
+<div class="card">
+    <h1>DKP System</h1>
+
+    The goal of the DKP system is to make loot distribution fast and at the same time fair. We will be adjusting things going forward so if you have any feedback, don't hesitate to contact one of the officers.
+
+    Your "Main" characters gain (0 to 1) points when they participate in an event, and spend (0 to 5) points to buy the items that drop from an event. 
+
+    <h2>Mains, Alts and Bots</h2>
+
+    Every player has 3 dedicated "Mains". These "Mains" will be priority number one on loot, followed by "Alts". We also have access to several "Bot" characters who are ranked behind "Alts" in Priority.
+    It is possible to change your "Main", however points are not transferable between characters. We reserve the right to set limits for the number of mainchanges/year if we see people switching too frequently.
+</div>
