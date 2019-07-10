fff85439a184f7dc2c8c061a85dee1aa7289377a
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-07-10 18:45:28 +0200

    removed trailing comma

--- assets/js/officer_panel.js
+++ assets/js/officer_panel.js
@@ -310,6 +310,6 @@ function get_list() {
     for (let i = 0; i < character_names.length; i++) {
         list += i+1 + " - " +character_names[i] +", ";
     }
-    list = list.slice(0, -1);
+    list = list.slice(0, -2);
     $("#winner_tbody").html("<tr><td id='random_list' class='align-middle'>List of eligible characters: "+list+"</td><td><a onclick='clipboard()' title='Copy to Clipboard' class='btn btn-sm btn-success mr-0'><i class='material-icons align-middle'>file_copy</i></a></td></tr>");
 }
