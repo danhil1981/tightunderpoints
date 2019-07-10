d2ce059e4c9a2736d70cb492a1ce756d85959d3b
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-07-10 18:17:06 +0200

    lastfix

--- assets/js/officer_panel.js
+++ assets/js/officer_panel.js
@@ -279,7 +279,7 @@ function get_winner() {
 function clipboard() {
     let clipboard = $("#random_list").html;
     clipboard.select();
-    document.execCommand(copy);
+    document.execCommand("copy");
 }
 
 function get_list() {
@@ -304,5 +304,6 @@ function get_list() {
     for (let i = 0; i < character_names.length; i++) {
         list += i+1 + " -> " +character_names[i] +", ";
     }
+    list = list.slice(0, -1);
     $("#winner_tbody").html("<tr><td id='random_list' class='align-middle'>List of eligible characters: "+list+"</td><td><a title='Copy to Clipboard' onclick='clipboard()' class='btn btn-sm btn-success mr-0'><i class='material-icons align-middle'>file_copy</i></a></td></tr>");
 }
