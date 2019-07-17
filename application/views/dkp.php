a53c31b20c656a9cb0fbf2433b29ce80dd12f0af
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-07-17 16:06:40 +0200

    some fixes, added item restriction section

--- application/views/dkp.php
+++ application/views/dkp.php
@@ -15,23 +15,24 @@
 }
 </style>
 <div class="col-10 mx-auto">
+
     <h1 class="text-center h1_text my-5">DKP System</h1>
     <p class="text-white text-justify">
-        The goal of the DKP system is to make loot distribution understandable, fast and fair.  The goal is for the people who show up the most to get the most loot, while still making sure that everyone has a chance to get some good stuff.
-        Points, costs and many other things are tracked on the tightunderpoints webpage, linked in the resources section on Discord.
-    </p>
-        
+        The goal of the DKP system is to make loot distribution understandable, fast and fair. 
+        All costs and rewards are posted to the members section of the website (Bosses and Items sections).
+        The points section of the website shows you the current standing of points and also lets you compare individual members.
+    </p>  
 
     <h2 class="h2_text">Earning Points</h2>
     <p class="text-white text-justify">
-        One point is earned for each boss kill during a pre-planned raid.  Additionally, the first kill on any new boss will earn a bonus point.  Earned points decay 50 days after they are earned.
+        One point is earned for each boss kill during a pre-scheduled raid.  Additionally, the first kill on any new boss will earn an extra bonus point. Earned points decay 50 days after they are earned.
         Points are earned separately for each character.  This ensures that loot is distributed fairly regardless of the number of characters played and also prevents a player from monopolizing loot on one character at the expense of their others.
         Smaller bosses may be killed on off nights if people are available and interested.  In order to be as fair as possible to those who aren't spending their time stalking discord, these kills will not earn points.
     </p>
     <h2 class="h2_text">Spending Points and Looting</h2>
     <p class="text-white text-justify">
         When an item drops, players will announce which item(s) they are interested in and which character they wish to loot on.  A /random will then be done between the character with the most points and anyone within two points of them and the winner will pay the point cost for the item.  
-        For example, suppose a 4 point item drops and three players are interested; LotLizard with 10 points, SheepLover with 8 points and ErbenZaehler with 6 points.  LotLizard has the most points and SheepLover is within 2 points of them, so they are eligible.  ErbenZaehler is not within 2 points of player1, so they are not eligible.  Whoever is handling loot will announce "LotLizard - 1, SheepLover - 2" then do a /random 1 2 to determine the winner.
+        For example, suppose a 4 point item drops and three players are interested; LotLizard with 10 points, SheepLover with 8 points and ErbenZaehler with 6 points.  LotLizard has the most points and SheepLover is within 2 points of them, so they are eligible.  ErbenZaehler is not within 2 points of Lotlizard, so they are not eligible.  Whoever is handling loot will announce "LotLizard - 1, SheepLover - 2" then do a /random 1 2 to determine the winner.
         Item costs will decay 50 days after they are looted.  Combined with the decay on points earned this means all looting is based on a 50 day (~6 week) moving window.
         If a boss kill isn't worth a point, whether because it is being killed on an off night or it is too small to earn a point (i.e. glyphed serpent or Rhag1), then its loot will also not cost points.  Additionally, some items are just so bad that we can't justify even a 1 point cost for them.  In either of these cases, the loot will be randomed without considering DKP.
     </p>
@@ -52,10 +53,9 @@
         This system was built with simplicity as a consideration.  While we tried to address them, we're sure there are ways to game the system.  Trying to abuse the system is a dick move.  Don't do it.  If you're too successful at it we'll just change the rules to close whatever loophole you're abusing anyway.
         That said, if you feel like the system is not achieving the goals we've laid out don't hesitate to raise any concerns.  Often there are trade-offs and a judgement call has to be made, but we're trying to make it as understandable, fast and fair as possible.
     </p>
-    <h2 class="h2_text">Rewards/Costs and Standings</h2>
+    <h2 class="h2_text">Common Sense restrictions on items</h2>
     <p class="text-white text-justify">
-        All costs and rewards are posted to the members section of the website. You can find them in the Bosses and Items tables.
-        The points section of the website shows you the current standing of points and also lets you compare individual members.
+        While we do not like imposing limits on who can loot what, there will be items that should go to a specific role in the interest of the guild and raid force. Aggro Weapons will go to Tanks first and foremost, Top FT Items go to Pure Casters or Bards (Fading Memories) before opening up to other Hybrids, etc.
     </p>
 </div>
 
