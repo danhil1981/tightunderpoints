bf3d2e8601c16e936004528f5239e0b881d5f592
Daniel Hilbrand <daniel.hilbrand@hotmail.com>
2019-07-21 19:57:46 +0200

    removed bonus from timers

--- application/views/tables/officers/timers.php
+++ application/views/tables/officers/timers.php
@@ -22,25 +22,27 @@
                                 <?php
                                     $timezone = +1;
                                     foreach ($timers as $value) {
-                                        echo "<tr><td class='align-middle'><div class='btn boss boss_" . $value['id_boss'] . "'>" . $value['name_boss'] . '</div></td>';
-                                        echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['last_killed'])) . '</td>';
-                                        echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['start_window'])) . '</td>';
-                                        echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['end_window'])) . '</td>';
-                                        echo "<td>" . $value['last_killed'] . '</td>';
-                                        echo "<td>" . $value['start_window'] . '</td>';
-                                        echo "<td>" . $value['end_window'] . '</td>';
-                                        echo "<td class='fit align-middle'>";
-                                        if (gmdate('Y-m-d H:i:s', time() + 3600 * ($timezone + date('I'))) > $value['end_window']) {
-                                            echo "<div title='Up' class='btn btn-sm btn-success'><i class='material-icons align-middle'>event_available</i></div></td><td class='fit align-middle'><a title='Track " . $value['name_boss'] . "' href='" . site_url() . 'events/show_insert/officers/' . $value['id_boss'] . "' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>alarm_add</i></a>";
-                                        } else {
-                                            if (gmdate('Y-m-d H:i:s', time() + 3600 * ($timezone + date('I'))) < $value['start_window']) {
-                                                echo "<div title='Down' class='btn btn-sm btn-danger'><i class='material-icons align-middle'>event_busy</i></div></td><td>";
+                                        if (strpos($value['name_boss'], 'Bonus') !== false) {
+                                            echo "<tr><td class='align-middle'><div class='btn boss boss_" . $value['id_boss'] . "'>" . $value['name_boss'] . '</div></td>';
+                                            echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['last_killed'])) . '</td>';
+                                            echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['start_window'])) . '</td>';
+                                            echo "<td class='align-middle'>" . date('D j M, H:i', strtotime($value['end_window'])) . '</td>';
+                                            echo '<td>' . $value['last_killed'] . '</td>';
+                                            echo '<td>' . $value['start_window'] . '</td>';
+                                            echo '<td>' . $value['end_window'] . '</td>';
+                                            echo "<td class='fit align-middle'>";
+                                            if (gmdate('Y-m-d H:i:s', time() + 3600 * ($timezone + date('I'))) > $value['end_window']) {
+                                                echo "<div title='Up' class='btn btn-sm btn-success'><i class='material-icons align-middle'>event_available</i></div></td><td class='fit align-middle'><a title='Track " . $value['name_boss'] . "' href='" . site_url() . 'events/show_insert/officers/' . $value['id_boss'] . "' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>alarm_add</i></a>";
                                             } else {
-                                                echo "<div title='In Window' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>indeterminate_check_box</i></div></td><td class='fit align-middle'><a title='Track " . $value['name_boss'] . "' href='" . site_url() . 'events/show_insert/officers/' . $value['id_boss'] . "' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>alarm_add</i></a>";
+                                                if (gmdate('Y-m-d H:i:s', time() + 3600 * ($timezone + date('I'))) < $value['start_window']) {
+                                                    echo "<div title='Down' class='btn btn-sm btn-danger'><i class='material-icons align-middle'>event_busy</i></div></td><td>";
+                                                } else {
+                                                    echo "<div title='In Window' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>indeterminate_check_box</i></div></td><td class='fit align-middle'><a title='Track " . $value['name_boss'] . "' href='" . site_url() . 'events/show_insert/officers/' . $value['id_boss'] . "' class='btn btn-sm btn-primary'><i class='material-icons align-middle'>alarm_add</i></a>";
+                                                }
                                             }
+                                            echo '</td>';
+                                            echo '</tr>';
                                         }
-                                        echo '</td>';
-                                        echo '</tr>';
                                     }
                                 ?>
                             </tbody>
